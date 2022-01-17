db71a507a53d6ca03385d2e0f5e732b3840601b2
SonOfMagic <qq1324318532@gmail.com>
2022-01-17 22:39:09 +0800

    chore: add test script

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,22 @@
-import type { Compiler } from 'webpack'
+import type { Compiler, Compilation } from 'webpack'
 // import { sources } from 'webpack'
 
-// const pluginName = 'weapp-tailwindcss-webpack-plugin'
+const pluginName = 'weapp-tailwindcss-webpack-plugin'
 
 export class WeappTailwindcssWebpackPlugin {
   apply (compiler: Compiler) {
-    console.log(compiler)
+    compiler.hooks.compilation.tap(pluginName, this.initializePlugin.bind(this))
+  }
+
+  initializePlugin (compilation: Compilation): void {
+    compilation.hooks.additionalAssets.tapPromise(pluginName, () => {
+      return this.runPluginHook(compilation)
+    })
+  }
+
+  async runPluginHook (compilation: Compilation): Promise<void> {
+    console.log(compilation.assets)
+
+    console.log(compilation.chunks)
   }
 }
