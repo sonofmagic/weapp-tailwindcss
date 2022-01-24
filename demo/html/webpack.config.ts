8774a49c2f403f5445585d20e8a7aac21d055d7b
SonOfMagic <qq1324318532@gmail.com>
2022-01-24 20:56:14 +0800

    feat: commit codes

--- demo/html/webpack.config.ts
+++ demo/html/webpack.config.ts
@@ -1,5 +1,6 @@
 import * as path from 'path'
 import * as webpack from 'webpack'
+import HtmlWebpackPlugin from 'html-webpack-plugin'
 // in case you run into any typescript error when configuring `devServer`
 import 'webpack-dev-server'
 
@@ -10,7 +11,25 @@ const config: webpack.Configuration = {
     path: path.resolve(__dirname, 'dist'),
     filename: 'main.js'
   },
-  devServer: {}
+  devServer: {},
+  devtool: false,
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          'style-loader',
+          // Translates CSS into CommonJS
+          'css-loader',
+          // Compiles Sass to CSS
+          'postcss-loader',
+          'sass-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [new HtmlWebpackPlugin()]
 }
 
 export default config
