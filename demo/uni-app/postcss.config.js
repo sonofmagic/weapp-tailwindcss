4aa5fe67c31e9025a16ac1ace6793220f057f480
SonOfMagic <qq1324318532@gmail.com>
2022-01-25 18:01:56 +0800

    feat: commit postcss 7 config

--- demo/uni-app/postcss.config.js
+++ demo/uni-app/postcss.config.js
@@ -17,6 +17,12 @@ module.exports = {
     require('autoprefixer')({
       remove: process.env.UNI_PLATFORM !== 'h5'
     }),
+    require('tailwindcss')({ config: './tailwind.config.js' }),
+    require('postcss-rem-to-responsive-pixel')({
+      rootValue: 32,
+      propList: ['*'],
+      transformUnit: 'rpx'
+    }),
     require('@dcloudio/vue-cli-plugin-uni/packages/postcss')
   ]
 }
