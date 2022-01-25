4f101677adb89901c812562098e6698e85cc22b0
SonOfMagic <qq1324318532@gmail.com>
2022-01-25 11:24:46 +0800

    feat: prepare for replacement

--- src/types.ts
+++ src/types.ts
@@ -1,12 +1,3 @@
 export interface UserDefinedOptions {
-  classNameRegExp?: string
-  reserveClassName?: string[]
-  ignorePrefix?: string[]
-  ignorePrefixRegExp?: string[]
-  classGenerator?: (
-    original: string,
-    opts: UserDefinedOptions,
-    context: Record<string, any>
-  ) => string
-  log?: boolean
+
 }
