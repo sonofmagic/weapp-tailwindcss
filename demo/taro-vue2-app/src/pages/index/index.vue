34a254864b4af8398e422a2736820fa1301737b0
SonOfMagic <qq1324318532@gmail.com>
2022-02-11 14:23:03 +0800

    feat: init taro vue2/3 version

---
+++ demo/taro-vue2-app/src/pages/index/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <view class="index">
+    <text>{{ msg }}</text>
+  </view>
+</template>
+
+<script>
+import './index.scss'
+
+export default {
+  data () {
+    return {
+      msg: 'Hello world!'
+    }
+  }
+}
+</script>
