78be630d2983ac3ff60776aa065f5bff1d4c7285
SonOfMagic <qq1324318532@gmail.com>
2022-02-17 00:35:11 +0800

    feat: add remax-app doc

---
+++ demo/remax-app/README.md
@@ -0,0 +1,8 @@
+## webpack
+
+webpack@^4.42.1:
+version "4.46.0"
+
+## postcss
+
+postcss "^7.0.2"
--- demo/remax-app/src/pages/index/index.js
+++ demo/remax-app/src/pages/index/index.js
@@ -1,21 +1,27 @@
-import * as React from 'react';
-import { View, Text, Image } from 'remax/one';
-import styles from './index.css';
+import * as React from 'react'
+import { View } from 'remax/one'
+import './index.css'
 
 export default () => {
   return (
-    <View className={styles.app}>
-      <View className={styles.header}>
-        <Image
-          src="https://gw.alipayobjects.com/mdn/rms_b5fcc5/afts/img/A*OGyZSI087zkAAAAAAAAAAABkARQnAQ"
-          className={styles.logo}
-          alt="logo"
-        />
-        <View className={styles.text}>
-          编辑 <Text className={styles.path}>src/pages/index/index.js</Text>{' '}
-          开始
+    <>
+      <View className="p-[20px] -mt-2 mb-[-20px] ">p-[20px] -mt-2 mb-[-20px] margin的jit 可不能这么写 -m-[20px]</View>
+      <View className="space-y-[1.6rem]">
+        <View className="w-[300rpx] text-black text-opacity-[0.19]">w-[300rpx] text-black text-opacity-[0.19]</View>
+        <View className="min-w-[300rpx] max-h-[100px] text-[20px] leading-[0.9]">min-w-[300rpx] max-h-[100px] text-[20px] leading-[0.9]</View>
+        <View className="max-w-[300rpx] min-h-[100px] text-[#dddddd]">max-w-[300rpx] min-h-[100px] text-[#dddddd]</View>
+        <View className="flex items-center justify-center h-[100px] w-[100px] rounded-[40px] bg-[#123456] bg-opacity-[0.54] text-[#ffffff]">Hello</View>
+        <View className="border-[10px] border-[#098765] border-solid border-opacity-[0.44]">border-[10px] border-[#098765] border-solid border-opacity-[0.44]</View>
+        <View className="grid grid-cols-3 divide-x-[10px] divide-[#010101] divide-solid">
+          <View>1</View>
+          <View>2</View>
+          <View>3</View>
+        </View>
+        <View className="w-32 py-2 rounded-md font-semibold text-white bg-pink-500 ring-4 ring-pink-300">
+          Default
         </View>
       </View>
-    </View>
-  );
-};
+      <View className="test">test</View>
+    </>
+  )
+}
