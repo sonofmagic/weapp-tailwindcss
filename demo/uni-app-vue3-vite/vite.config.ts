30997b0d344f8ab208afbeb1842854fa9310ee85
SonOfMagic <qq1324318532@gmail.com>
2022-03-30 14:13:13 +0800

    feat: h5 compat

--- demo/uni-app-vue3-vite/vite.config.ts
+++ demo/uni-app-vue3-vite/vite.config.ts
@@ -4,7 +4,7 @@ import uni from '@dcloudio/vite-plugin-uni';
 
 const isH5 = process.env.UNI_PLATFORM === 'h5';
 
-import { ViteWeappTailwindcssPlugin as vwt, postcssWeappTailwindcssRename } from '../..';
+import { ViteWeappTailwindcssPlugin as vwt } from '../..';
 // import { ViteWeappTailwindcssPlugin as vwt, postcssWeappTailwindcssRename } from 'weapp-tailwindcss-webpack-plugin'
 //
 // https://vitejs.dev/config/
