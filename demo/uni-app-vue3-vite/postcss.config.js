cad6e923e7fa4add598d662c4f71c72aba95507c
SonOfMagic <qq1324318532@gmail.com>
2022-03-30 14:10:52 +0800

    feat: commit codes

--- demo/uni-app-vue3-vite/postcss.config.js
+++ demo/uni-app-vue3-vite/postcss.config.js
@@ -1,14 +1,17 @@
 // 假如不起作用，请使用内联postcss
+const isH5 = process.env.UNI_PLATFORM === 'h5';
+
 module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-    'postcss-rem-to-responsive-pixel': {
-      rootValue: 32,
-      propList: ['*'],
-      transformUnit: 'rpx'
-    },
-    //'../../postcss': {}
-    'weapp-tailwindcss-webpack-plugin/postcss': {}
-  }
-}
+  plugins: [
+    require('autoprefixer')(),
+    require('tailwindcss')(),
+    isH5
+      ? undefined
+      : require('postcss-rem-to-responsive-pixel')({
+          rootValue: 32,
+          propList: ['*'],
+          transformUnit: 'rpx'
+        }),
+    isH5 ? undefined : require('weapp-tailwindcss-webpack-plugin/postcss')()
+  ]
+};
--- demo/uni-app-vue3-vite/vite.config.ts
+++ demo/uni-app-vue3-vite/vite.config.ts
@@ -1,28 +1,13 @@
 import { defineConfig } from 'vite';
 import uni from '@dcloudio/vite-plugin-uni';
+// console.log(process.env.UNI_PLATFORM);
+
+const isH5 = process.env.UNI_PLATFORM === 'h5';
 
 import { ViteWeappTailwindcssPlugin as vwt, postcssWeappTailwindcssRename } from '../..';
 // import { ViteWeappTailwindcssPlugin as vwt, postcssWeappTailwindcssRename } from 'weapp-tailwindcss-webpack-plugin'
 //
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [uni(), vwt()],
-  css: {
-    postcss: {
-      plugins: [
-        require('autoprefixer')(),
-        require('tailwindcss')(),
-        require('postcss-rem-to-responsive-pixel')({
-          rootValue: 32,
-          propList: ['*'],
-          transformUnit: 'rpx'
-        }),
-        postcssWeappTailwindcssRename({
-          // cssPreflight: {
-          //   'box-sizing': false
-          // }
-        })
-      ]
-    }
-  }
+  plugins: [uni(), isH5 ? undefined : vwt()]
 });
