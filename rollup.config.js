efb853f577e5def28ccf053d5a47a353b49f84ac
SonOfMagic <qq1324318532@gmail.com>
2022-03-30 14:42:07 +0800

    v1.2.9

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,7 @@
 import typescript from '@rollup/plugin-typescript'
 import { nodeResolve } from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
-import { terser } from 'rollup-plugin-terser'
+// import { terser } from 'rollup-plugin-terser'
 import pkg from './package.json'
 // const isProd = process.env.NODE_ENV === 'production'
 const isDev = process.env.NODE_ENV === 'development'
@@ -24,14 +24,14 @@ const config = {
       preferBuiltins: true
     }),
     commonjs(),
-    typescript({ tsconfig: './tsconfig.build.json', sourceMap: isDev, declaration: false }),
-    isDev
-      ? undefined
-      : terser({
-        format: {
-          comments: false
-        }
-      })
+    typescript({ tsconfig: './tsconfig.build.json', sourceMap: isDev, declaration: false })
+    // isDev
+    //   ? undefined
+    //   : terser({
+    //     format: {
+    //       comments: false,
+    //     }
+    //   })
   ],
   external: [...(pkg.dependencies ? Object.keys(pkg.dependencies) : [])]
 }
