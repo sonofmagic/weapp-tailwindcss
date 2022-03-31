59d6b127a1b63555d9727581ad7335852a29516c
SonOfMagic <qq1324318532@gmail.com>
2022-03-31 18:01:15 +0800

    fix: add inline postcss comment

--- demo/uni-app-vue3-vite/vite.config.ts
+++ demo/uni-app-vue3-vite/vite.config.ts
@@ -4,10 +4,25 @@ import uni from '@dcloudio/vite-plugin-uni';
 
 const isH5 = process.env.UNI_PLATFORM === 'h5';
 
-import { ViteWeappTailwindcssPlugin as vwt } from '../..';
+import { ViteWeappTailwindcssPlugin as vwt, postcssWeappTailwindcssRename } from '../..';
 // import { ViteWeappTailwindcssPlugin as vwt, postcssWeappTailwindcssRename } from 'weapp-tailwindcss-webpack-plugin'
 //
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [uni(), isH5 ? undefined : vwt()]
+  // 假如 postcss.config.js 不起作用，请使用内联 postcss
+  // css: {
+  //   postcss: {
+  //     plugins: [
+  //       require('autoprefixer')(),
+  //       require('tailwindcss')(),
+  //       require('postcss-rem-to-responsive-pixel')({
+  //         rootValue: 32,
+  //         propList: ['*'],
+  //         transformUnit: 'rpx'
+  //       }),
+  //       postcssWeappTailwindcssRename()
+  //     ]
+  //   }
+  // }
 });
