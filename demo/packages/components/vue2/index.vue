e3e67a244de00cab6ffb84d84ed89bd8dd4a580d
SonOfMagic <1324318532@qq.com>
2022-04-21 00:34:47 +0800

    feat: remove work space

---
+++ demo/packages/components/vue2/index.vue
@@ -0,0 +1,64 @@
+<template>
+  <div>
+    <view class="p-[20px] -mt-2 mb-[-20px] h-[200%]">p-[20px] -mt-2 mb-[-20px] margin的jit 可不能这么写 -m-[20px]</view>
+    <view class="space-y-[1.6rem]">
+      <view class="w-[300rpx] text-black text-opacity-[0.19]">w-[300rpx] text-black text-opacity-[0.19]</view>
+      <view class="min-w-[300rpx] max-h-[100px] text-[20px] leading-[0.9]">min-w-[300rpx] max-h-[100px] text-[20px]
+        leading-[0.9]</view>
+      <view class="max-w-[300rpx] min-h-[100px] text-[#dddddd]">max-w-[300rpx] min-h-[100px] text-[#dddddd]</view>
+      <view
+        class="flex items-center justify-center h-[100px] w-[100px] rounded-[40px] bg-[#123456] bg-opacity-[0.54] text-[#ffffff]">
+        Hello</view>
+      <view class="border-[10px] border-[#098765] border-solid border-opacity-[0.44]">border-[10px] border-[#098765]
+        border-solid border-opacity-[0.44]</view>
+      <view class="grid grid-cols-3 divide-x-[10px] divide-[#010101] divide-solid">
+        <div>1</div>
+        <div>2</div>
+        <div>3</div>
+      </view>
+      <view class="w-32 py-2 rounded-md font-semibold text-white bg-pink-500 ring-4 ring-pink-300">
+        Default
+      </view>
+    </view>
+    <view class="test">test</view>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  data() {
+    return {
+      displayArray: [
+        'block',
+        'inline-block',
+        'inline',
+        'flex',
+        'inline-flex',
+        'table',
+        'inline-table',
+        'table-caption',
+        'table-cell',
+        'table-column',
+        'table-column-group',
+        'table-footer-group',
+        'table-header-group',
+        'table-row-group',
+        'table-row',
+        'flow-root',
+        'grid',
+        'inline-grid',
+        'contents',
+        'list-item',
+        'hidden'
+      ]
+    }
+  }
+})
+</script>
+<style lang="scss">
+.test {
+  @apply flex items-center justify-center h-[100px] w-[100px] rounded-[40px] bg-[#123456] bg-opacity-[0.54] text-[#ffffff] #{!important};
+}
+</style>
+
