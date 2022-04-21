5d243d65cde3098ba6e2dbfc76b75dd62812c329
SonOfMagic <1324318532@qq.com>
2022-04-21 14:58:12 +0800

    fix: add vite test case

--- demo/uni-app-vue3-vite/src/pages/index/index.vue
+++ demo/uni-app-vue3-vite/src/pages/index/index.vue
@@ -1,5 +1,7 @@
 <template>
   <view class="content">
+    <view class="w-2 h-2 bg-[#123456]"></view>
+    <view class="w-2 h-2 bg-blue-500/50"></view>
     <view class="flex items-center justify-center w-screen h-screen">
       <view class="!font-bold !text-[#990000]" :class="['text-2xl', { underline: true }]">{{ title }}</view>
     </view>
@@ -10,14 +12,14 @@
     <view class="p-[20px] -mt-2 mb-[-20px]">p-[20px] -mt-2 mb-[-20px] margin的jit 可不能这么写 -m-[20px]</view>
     <view class="space-y-[1.6rem]">
       <view class="w-[300rpx] text-black text-opacity-[0.19]">w-[300rpx] text-black text-opacity-[0.19]</view>
-      <view class="min-w-[300rpx] max-h-[100px] text-[20px] leading-[0.9]">min-w-[300rpx] max-h-[100px] text-[20px] leading-[0.9]</view>
+      <view class="min-w-[300rpx] max-h-[100px] text-[20px] leading-[0.9]">min-w-[300rpx] max-h-[100px] text-[20px]
+        leading-[0.9]</view>
       <view class="max-w-[300rpx] min-h-[100px] text-[#dddddd]">max-w-[300rpx] min-h-[100px] text-[#dddddd]</view>
       <view
         class="flex items-center justify-center h-[100px] w-[100px] rounded-[40px] bg-[#123456] bg-opacity-[0.54] text-[#ffffff]"
-        :class="['text-[#123456]', flag ? 'bg-[#666600]' : 'bg-[#410000]']"
-        >Hello</view
-      >
-      <view class="border-[10px] border-[#098765] border-solid border-opacity-[0.44]">border-[10px] border-[#098765] border-solid border-opacity-[0.44]</view>
+        :class="['text-[#123456]', flag ? 'bg-[#666600]' : 'bg-[#410000]']">Hello</view>
+      <view class="border-[10px] border-[#098765] border-solid border-opacity-[0.44]">border-[10px] border-[#098765]
+        border-solid border-opacity-[0.44]</view>
       <view class="grid grid-cols-3 divide-x-[10px] divide-[#010101] divide-solid">
         <div>1</div>
         <div>2</div>
