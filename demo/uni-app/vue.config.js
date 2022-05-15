4591d46e84b6a5f863f6a834e1d028e1db03c348
SonOfMagic <1324318532@qq.com>
2022-05-15 15:23:50 +0800

    feat: commit demos

--- demo/uni-app/vue.config.js
+++ demo/uni-app/vue.config.js
@@ -1,15 +1,18 @@
 const { UniAppWeappTailwindcssWebpackPluginV4 } = require('../..')
+const SpeedMeasurePlugin = require('speed-measure-webpack-plugin')
+const smp = new SpeedMeasurePlugin({
+  //outputTarget: './smp.dat',
+})
 // const { UniAppWeappTailwindcssWebpackPluginV4 } = require('weapp-tailwindcss-webpack-plugin')
 /**
  * @type {import('@vue/cli-service').ProjectOptions}
  */
 const config = {
-  configureWebpack: {
-    plugins: [new UniAppWeappTailwindcssWebpackPluginV4({
-      cssPreflight: {
-        // 'box-sizing': false
-      }
-    })]
+  configureWebpack: (config) => {
+    smp.wrap(config)
+  },
+  chainWebpack: (config) => {
+    config.plugin('UniAppWeappTailwindcssWebpackPluginV4').use(UniAppWeappTailwindcssWebpackPluginV4).end()
   }
 }
 
