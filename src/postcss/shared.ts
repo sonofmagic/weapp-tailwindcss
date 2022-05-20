126777fae40be50172f102e111ffc80ed60d8de8
SonOfMagic <qq1324318532@gmail.com>
2022-05-20 15:12:13 +0800

    feat: commit SYMBOL_TABLE & MappingChars2String

--- src/postcss/shared.ts
+++ src/postcss/shared.ts
@@ -1,22 +1,21 @@
-// .sm\\:text-3xl
+import { MappingChars2String as dic } from '@/dic'
 // css 中，要多加一个 '\' 来转义
 export function cssSelectorReplacer (selector: string) {
   return (
     selector
-      .replace(/\\\[/g, '_l_') // \[
-      .replace(/\\\]/g, '_r_') // \]
-      .replace(/\\\(/g, '_p_') // \(
-      .replace(/\\\)/g, '_q_') // \)
-      .replace(/\\#/g, '_h_') // \# : hex
-      .replace(/\\!/g, '_i_') // \! : !important
-      .replace(/\\\//g, '_div_') // \/ : w-1/2 -> width:50%
-      .replace(/\\\./g, '_dot_') // \. : w-1.5
-      // \\:
-      .replace(/\\:/g, '_c_') // colon for screen
+      .replace(/\\\[/g, dic['[']) // \[
+      .replace(/\\\]/g, dic[']']) // \]
+      .replace(/\\\(/g, dic['(']) // \(
+      .replace(/\\\)/g, dic[')']) // \)
+      .replace(/\\#/g, dic['#']) // \# : hex
+      .replace(/\\!/g, dic['!']) // \! : !important
+      .replace(/\\\//g, dic['/']) // \/ : w-1/2 -> width:50%
+      .replace(/\\\./g, dic['.']) // \. : w-1.5
+      .replace(/\\:/g, dic[':']) // colon for screen
       // https://github.com/sonofmagic/weapp-tailwindcss-webpack-plugin/issues/8
-      .replace(/\\%/g, '_pct_')
+      .replace(/\\%/g, dic['%'])
       // .replace(/\\,/g, '_d_')
-      .replace(/\\2c /g, '_d_')
-      .replace(/\\'/g, '_y_')
+      .replace(/\\2c /g, dic[','])
+      .replace(/\\'/g, dic["'"])
   )
 }
