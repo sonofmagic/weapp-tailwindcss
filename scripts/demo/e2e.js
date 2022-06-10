4670a3ccc807313afb187abcf13b5ee95ddbb2bb
SonOfMagic <qq1324318532@gmail.com>
2022-06-10 14:20:06 +0800

    e2e: prepare for pages/index/index route

--- scripts/demo/e2e.js
+++ scripts/demo/e2e.js
@@ -21,10 +21,11 @@ async function main () {
     const projectPath = projectPaths[index]
     await automator
       .launch({
+        cliPath: 'D:\\Program Files (x86)\\Tencent\\微信web开发者工具\\cli.bat',
         projectPath
       })
       .then(async (miniProgram) => {
-        const page = await miniProgram.reLaunch()
+        const page = await miniProgram.reLaunch('/page/index/index')
         await page.waitFor(10_000)
         // const element = await page.$('.kind-list-item-hd')
         // console.log(await element.attribute('class'))
