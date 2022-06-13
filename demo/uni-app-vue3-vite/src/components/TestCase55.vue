cf89150df3a4224fff331da02ecc844e8b31ace9
SonOfMagic <qq1324318532@gmail.com>
2022-06-13 10:38:35 +0800

    feat: commit test case for TestCase55

---
+++ demo/uni-app-vue3-vite/src/components/TestCase55.vue
@@ -0,0 +1,36 @@
+<template>
+  <view :class="[classname, 'overflow-hidden', 'relative']">https://github.com/sonofmagic/weapp-tailwindcss-webpack-plugin/issues/55</view>
+</template>
+
+<script setup lang="ts">
+import { replaceJs } from 'weapp-tailwindcss-webpack-plugin/replace';
+import { ref, watch } from 'vue';
+
+interface Props {
+  className?: string;
+  src?: string;
+}
+const props = withDefaults(defineProps<Props>(), {
+  className: '',
+  src: '',
+});
+
+const emit = defineEmits(['click']);
+
+const show = ref<boolean>(false);
+const transition = ref<boolean>(true);
+const classname = ref<string>('');
+
+watch(
+  () => props.className,
+  (nv, ov) => {
+    classname.value = replaceJs(props.className);
+  },
+  {
+    immediate: true,
+  }
+);
+</script>
+
+<style scoped>
+</style>
---
+++ demo/uni-app-vue3-vite/src/pages/issue/case55.vue
@@ -0,0 +1,22 @@
+<template>
+  <view>
+    <TestCase55 className="h-[111px] w-[222px] bg-[#987654]"></TestCase55>
+
+    <TestCase55 :className="className"></TestCase55>
+
+    <button @click="toggleClass">toggleClass</button>
+  </view>
+</template>
+
+<script setup lang="ts">
+import TestCase55 from '@/components/TestCase55.vue';
+import { ref } from 'vue';
+const className = ref<string>('h-[88.88px] w-[33.33px] bg-[yellow]');
+
+const toggleClass = () => {
+  className.value = 'h-[128px] w-[242px] bg-[#955443]';
+};
+</script>
+
+<style scoped>
+</style>
