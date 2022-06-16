ae8387858beff8eb72ddc320213ecb150562dbff
SonOfMagic <qq1324318532@gmail.com>
2022-06-16 16:28:34 +0800

    feat: commit codes

---
+++ demo/uni-app-vue3-vite/src/subs/demo/pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <view class="text-[56.5rpx] text-[#ab1932]"
+    >1234567890
+    <view class="text-[77rpx] text-[#abcdef]">你好啊</view>
+  </view>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped>
+</style>
