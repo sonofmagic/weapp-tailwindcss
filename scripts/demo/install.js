66a19fd9a9d72cec5c993d1589352bb7d24554d4
SonOfMagic <qq1324318532@gmail.com>
2022-06-24 15:43:32 +0800

    demo: upgrade demo

--- scripts/demo/install.js
+++ scripts/demo/install.js
@@ -5,4 +5,6 @@ const { install } = require('@icebreakers/cli')
   const demoPath = path.resolve(__dirname, '../../demo')
   await install(demoPath)
   await install(demoPath, '-D weapp-tailwindcss-webpack-plugin@latest tailwindcss-rem2px-preset@latest postcss-rem-to-responsive-pixel@latest')
+
+  await install(demoPath, '-D @icebreakers/weapp-tailwindcss-test-components')
 })()
