13924f20c2f3b94f3ee67cdef1394347adb143d8
SonOfMagic <qq1324318532@gmail.com>
2022-07-29 16:33:22 +0800

    release: 10-beta.0

--- scripts/demo/install.js
+++ scripts/demo/install.js
@@ -4,7 +4,7 @@ const { install } = require('@icebreakers/cli')
 ;(async () => {
   const demoPath = path.resolve(__dirname, '../../demo')
   await install(demoPath, undefined, true)
-  await install(demoPath, '-D weapp-tailwindcss-webpack-plugin@latest tailwindcss-rem2px-preset@latest postcss-rem-to-responsive-pixel@latest', true)
+  await install(demoPath, '-D weapp-tailwindcss-webpack-plugin@latest tailwindcss-rem2px-preset@latest postcss-rem-to-responsive-pixel@latest weapp-ide-cli@latest', true)
 
   await install(demoPath, '-D @icebreakers/weapp-tailwindcss-test-components', true)
 })()
