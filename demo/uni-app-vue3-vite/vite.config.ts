139d5ea597745fd7f435f448d156105f1cdda252
SonOfMagic <1324318532@qq.com>
2022-07-31 01:20:36 +0800

    chore: upgrade vite-demo to lts

--- demo/uni-app-vue3-vite/vite.config.ts
+++ demo/uni-app-vue3-vite/vite.config.ts
@@ -4,10 +4,8 @@ import uni from '@dcloudio/vite-plugin-uni';
 // import postcssWeappTailwindcssRename from 'weapp-tailwindcss-webpack-plugin/postcss';
 import vwt from '../../vite';
 import postcssWeappTailwindcssRename from '../../postcss';
-// console.log(process.env.UNI_PLATFORM);
 
 const isH5 = process.env.UNI_PLATFORM === 'h5';
-// import { ViteWeappTailwindcssPlugin as vwt, postcssWeappTailwindcssRename } from 'weapp-tailwindcss-webpack-plugin'
 
 // vite 插件配置
 const vitePlugins = [uni()];
@@ -16,13 +14,13 @@ const postcssPlugins = [require('autoprefixer')(), require('tailwindcss')()];
 if (!isH5) {
   vitePlugins.push(vwt());
 
-  // postcssPlugins.push(
-  //   require('postcss-rem-to-responsive-pixel')({
-  //     rootValue: 32,
-  //     propList: ['*'],
-  //     transformUnit: 'rpx',
-  //   })
-  // );
+  postcssPlugins.push(
+    require('postcss-rem-to-responsive-pixel')({
+      rootValue: 32,
+      propList: ['*'],
+      transformUnit: 'rpx',
+    })
+  );
   postcssPlugins.push(
     postcssWeappTailwindcssRename({
       // cssPreflight: {
@@ -40,20 +38,4 @@ export default defineConfig({
       plugins: postcssPlugins,
     },
   },
-
-  // 假如 postcss.config.js 不起作用，请使用内联 postcss
-  // css: {
-  //   postcss: {
-  //     plugins: [
-  //       require('autoprefixer')(),
-  //       require('tailwindcss')(),
-  //       require('postcss-rem-to-responsive-pixel')({
-  //         rootValue: 32,
-  //         propList: ['*'],
-  //         transformUnit: 'rpx'
-  //       }),
-  //       postcssWeappTailwindcssRename()
-  //     ]
-  //   }
-  // }
 });
