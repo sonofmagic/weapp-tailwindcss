923ea781f3dc75900f1ead07b42a3c7558358c4c
SonOfMagic <qq1324318532@gmail.com>
2022-08-04 15:14:04 +0800

    chore: upgrade taro to 3.5.1

--- demo/taro-app/config/index.js
+++ demo/taro-app/config/index.js
@@ -1,42 +1,43 @@
-const { TaroWeappTailwindcssWebpackPluginV4 } = require('../../../')
+const { TaroWeappTailwindcssWebpackPluginV5 } = require('../../../dist/index')
 // const { TaroWeappTailwindcssWebpackPluginV4 } = require('weapp-tailwindcss-webpack-plugin')
 const config = {
-  projectName: "myApp",
-  date: "2022-2-5",
+  compiler: 'webpack5',
+  projectName: 'myApp',
+  date: '2022-2-5',
   designWidth: 750,
   deviceRatio: {
     640: 2.34 / 2,
     750: 1,
-    828: 1.81 / 2,
+    828: 1.81 / 2
   },
-  sourceRoot: "src",
-  outputRoot: "dist",
+  sourceRoot: 'src',
+  outputRoot: 'dist',
   plugins: [],
   defineConstants: {},
   copy: {
     patterns: [],
-    options: {},
+    options: {}
   },
-  framework: "react",
+  framework: 'react',
   mini: {
     postcss: {
       pxtransform: {
         enable: true,
-        config: {},
+        config: {}
       },
       url: {
         enable: true,
         config: {
-          limit: 1024, // 设定转换尺寸上限
-        },
+          limit: 1024 // 设定转换尺寸上限
+        }
       },
       cssModules: {
         enable: false, // 默认为 false，如需使用 css modules 功能，则设为 true
         config: {
-          namingPattern: "module", // 转换模式，取值为 global/module
-          generateScopedName: "[name]__[local]___[hash:base64:5]",
-        },
-      },
+          namingPattern: 'module', // 转换模式，取值为 global/module
+          generateScopedName: '[name]__[local]___[hash:base64:5]'
+        }
+      }
       // tailwindcss: {},
       // autoprefixer: {},
     },
@@ -44,40 +45,42 @@ const config = {
       chain.merge({
         plugin: {
           install: {
-            plugin: TaroWeappTailwindcssWebpackPluginV4,
-            args: [{
-              framework: 'react',
-              // cssPreflight: {
-              //   'box-sizing': false
-              // }
-            }]
+            plugin: TaroWeappTailwindcssWebpackPluginV5,
+            args: [
+              {
+                framework: 'react'
+                // cssPreflight: {
+                //   'box-sizing': false
+                // }
+              }
+            ]
           }
         }
       })
     }
   },
   h5: {
-    publicPath: "/",
-    staticDirectory: "static",
+    publicPath: '/',
+    staticDirectory: 'static',
     postcss: {
       autoprefixer: {
         enable: true,
-        config: {},
+        config: {}
       },
       cssModules: {
         enable: false, // 默认为 false，如需使用 css modules 功能，则设为 true
         config: {
-          namingPattern: "module", // 转换模式，取值为 global/module
-          generateScopedName: "[name]__[local]___[hash:base64:5]",
-        },
-      },
-    },
-  },
-};
+          namingPattern: 'module', // 转换模式，取值为 global/module
+          generateScopedName: '[name]__[local]___[hash:base64:5]'
+        }
+      }
+    }
+  }
+}
 
 module.exports = function (merge) {
-  if (process.env.NODE_ENV === "development") {
-    return merge({}, config, require("./dev"));
+  if (process.env.NODE_ENV === 'development') {
+    return merge({}, config, require('./dev'))
   }
-  return merge({}, config, require("./prod"));
-};
+  return merge({}, config, require('./prod'))
+}
