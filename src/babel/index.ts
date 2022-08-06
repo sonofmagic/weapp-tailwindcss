bb848e9f8f73d68552476fa231e5a0b9ef32c020
sonofmagic <1324318532@qq.com>
2022-08-06 17:03:00 +0800

    v1.7.1-beta.0

--- src/babel/index.ts
+++ src/babel/index.ts
@@ -1,5 +1,9 @@
-import { parseExpression, parse } from '@babel/parser'
-import traverse from '@babel/traverse'
-import generate from '@babel/generator'
+export { default as generate } from './generate'
+export { default as parseExpression } from './parseExpression'
+export { default as parse } from './parse'
+export { default as traverse } from './traverse'
+// import { parseExpression, parse } from '@babel/parser'
+// import traverse from '@babel/traverse'
+// import generate from '@babel/generator'
 
-export { parseExpression, parse, traverse, generate }
+// export { parseExpression, parse, traverse, generate }
---
+++ src/babel/parse.ts
@@ -0,0 +1,3 @@
+import { parse } from '@babel/parser'
+
+export default parse
