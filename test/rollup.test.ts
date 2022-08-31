55f301a19ca55cd4d7e0564d6c1f1bf47b9f76a0
SonOfMagic <qq1324318532@gmail.com>
2022-08-31 17:59:28 +0800

    test: add build lib case

--- test/rollup.test.ts
+++ test/rollup.test.ts
@@ -1,18 +1,35 @@
 import configs from '../rollup.config'
 import { rollup } from 'rollup'
+import { omit } from 'lodash'
+// import type { RollupBuild } from 'rollup'
 describe('rollup build', () => {
   it('lib build', async () => {
-    const result = []
+    // const result:RollupBuild[] = []
     for (let i = 0; i < configs.length; i++) {
       const config = configs[i]
-
-      result.push(await rollup({
+      const bundle = await rollup({
         input: config.input,
         external: config.external,
         output: config.output,
         plugins: config.plugins
-      }))
+      })
+      if (Array.isArray(config.output)) {
+        for (let j = 0; j < config.output.length; j++) {
+          const { output } = await bundle.generate(config.output[j])
+          expect(
+            output.map((x) => {
+              return omit(x, ['modules', 'facadeModuleId'])
+            })
+          ).toMatchSnapshot()
+        }
+      } else if (config.output) {
+        const { output } = await bundle.generate(config.output)
+        expect(
+          output.map((x) => {
+            return omit(x, ['modules', 'facadeModuleId'])
+          })
+        ).toMatchSnapshot()
+      }
     }
-    expect(result.length).toBe(configs.length)
   })
 })
