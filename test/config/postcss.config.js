97dedde425af24d5210815f7610085a7a1733829
SonOfMagic <qq1324318532@gmail.com>
2022-09-05 12:17:07 +0800

    feat: commit baseProcessor and baseCss

--- test/config/postcss.config.js
+++ test/config/postcss.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   plugins: {
-    tailwindcss: {},
     autoprefixer: {},
+    tailwindcss: {},
     'postcss-rem-to-responsive-pixel': {
       rootValue: 32,
       propList: ['*'],
--- test/postcss.test.ts
+++ test/postcss.test.ts
@@ -1,5 +1,24 @@
+import postcss from 'postcss'
+import path from 'path'
+import fs from 'fs'
 describe('postcss plugin', () => {
-  it('should ', () => {
-    expect(true).toBeTruthy()
+  const baseProcessor = postcss([
+    require('autoprefixer')(),
+    require('tailwindcss')({ config: path.resolve(__dirname, './config/tailwind.config.js') }),
+    require('postcss-rem-to-responsive-pixel')({
+      rootValue: 32,
+      propList: ['*'],
+      transformUnit: 'rpx'
+    })
+  ])
+  const baseCss = fs.readFileSync(path.resolve(__dirname, './config/base.css'), {
+    encoding: 'utf8'
+  })
+  it('base tw output', async () => {
+    const res = await baseProcessor.process(baseCss, {
+      from: 'index.css',
+      to: 'index.css'
+    })
+    expect(res.css.toString()).toMatchSnapshot()
   })
 })
