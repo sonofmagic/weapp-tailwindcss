5b5cef9d9ac1b63c8df1c96ca59fcd285cc09d05
sonofmagic <1324318532@qq.com>
2022-09-06 22:36:49 +0800

    refact: move replacer?.end() into jsxHandler

--- src/jsx/index.ts
+++ src/jsx/index.ts
@@ -2,6 +2,8 @@ import { parse, traverse, generate } from '@/babel'
 import type { Replacer } from './replacer'
 
 export function jsxHandler (rawSource: string, replacer: Replacer) {
+  // https://github.com/sonofmagic/weapp-tailwindcss-webpack-plugin/issues/53
+  replacer?.end()
   const ast = parse(rawSource, {
     sourceType: 'unambiguous'
   })
