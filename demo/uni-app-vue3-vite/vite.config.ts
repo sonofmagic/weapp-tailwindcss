93ac86d9872a2e750a3057b118dc16062bc5d06b
SonOfMagic <qq1324318532@gmail.com>
2022-09-08 09:32:00 +0800

    chore: vite test case commit

--- demo/uni-app-vue3-vite/vite.config.ts
+++ demo/uni-app-vue3-vite/vite.config.ts
@@ -15,11 +15,7 @@ const vitePlugins = [uni()];
 const postcssPlugins = [require('autoprefixer')(), require('tailwindcss')()];
 if (!WeappTailwindcssDisabled) {
   vitePlugins.push(
-    vwt({
-      mangle: {
-        ignoreClass: ['bg-[#4268EA]', 'bg-[#123456]'],
-      },
-    })
+    vwt()
   );
 
   postcssPlugins.push(
