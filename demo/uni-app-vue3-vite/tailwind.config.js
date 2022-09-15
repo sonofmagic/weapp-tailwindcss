6ccc5bab9fdf725f58d14ce5fbb794e08887650b
SonOfMagic <qq1324318532@gmail.com>
2022-09-15 18:00:19 +0800

    feat: commit variant plugin

--- demo/uni-app-vue3-vite/tailwind.config.js
+++ demo/uni-app-vue3-vite/tailwind.config.js
@@ -1,32 +1,32 @@
-const plugin = require('tailwindcss/plugin');
-const { each, variants } = require('./variants.js');
-const fs = require('fs');
+const plugin = require('tailwindcss/plugin')
+const { each, variants } = require('./variants.js')
+const fs = require('fs')
 fs.writeFileSync('./variants.json', JSON.stringify(variants, null, 2), {
-  encoding: 'utf-8',
-});
+  encoding: 'utf-8'
+})
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./index.html', './src/**/*.{html,js,ts,jsx,tsx,vue}'],
   theme: {
-    extend: {},
+    extend: {}
   },
   plugins: [
     plugin(({ addVariant }) =>
       each((x) => {
-        addVariant(...x);
+        addVariant(...x)
         // addVariant('optional', '&:optional');
         // addVariant('group-optional', ':merge(.group):optional &');
         // addVariant('peer-optional', ':merge(.peer):optional ~ &');
       })
-    ),
+    )
   ],
   presets: [
     require('tailwindcss-rem2px-preset').createPreset({
       fontSize: 32,
-      unit: 'rpx',
-    }),
+      unit: 'rpx'
+    })
   ],
   corePlugins: {
-    preflight: false,
-  },
-};
+    preflight: false
+  }
+}
