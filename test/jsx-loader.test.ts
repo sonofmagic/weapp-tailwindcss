6901e6b77b78aa74b609c381c73f493e4800bc51
sonofmagic <1324318532@qq.com>
2022-10-02 02:47:51 +0800

    chore: change loader middle tmp asset's name

--- test/jsx-loader.test.ts
+++ test/jsx-loader.test.ts
@@ -10,13 +10,13 @@ const getVue2Case = createGetCase(loaderCasePath + '/taro-vue2-app')
 describe('jsx-loader handler', () => {
   // https://github.com/sonofmagic/weapp-tailwindcss-webpack-plugin/issues/53
   it('vue3 jsx normal case', async () => {
-    const code = await getVue3Case('First.tsx.snap')
+    const code = await getVue3Case('First.tsx.tmp')
     const result = jsxHandler(code, createReplacer('vue3'))
     expect(result).toMatchSnapshot()
   })
 
   it('new vue3 jsx normal case', async () => {
-    const code = await getVue3Case('First.tsx.snap')
+    const code = await getVue3Case('First.tsx.tmp')
     const result = newJsxHandler(code, 'vue3')
     const result2 = jsxHandler(code, createReplacer('vue3'))
     expect(result).toStrictEqual(result2)
@@ -24,7 +24,7 @@ describe('jsx-loader handler', () => {
   })
 
   it('new vue2 jsx normal case', async () => {
-    const code = await getVue2Case('render.tsx.snap')
+    const code = await getVue2Case('render.tsx.tmp')
     const result = newJsxHandler(code, 'vue2')
     expect(result).toMatchSnapshot()
   })
