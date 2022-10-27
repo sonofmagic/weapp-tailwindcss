2263265a3c48d8cf77a938df3cecdc48626ae053
SonOfMagic <qq1324318532@gmail.com>
2022-10-27 18:08:27 +0800

    chore: upgrade @dcloudio/uni

--- demo/uni-app-vue3-vite/_postcss.config.js
+++ demo/uni-app-vue3-vite/_postcss.config.js
@@ -1,20 +1,22 @@
 // 假如不起作用，请使用内联postcss
 const isH5 = process.env.UNI_PLATFORM === 'h5';
+const isApp = process.env.UNI_PLATFORM === 'app';
+const WeappTailwindcssDisabled = isH5 || isApp;
 
 const plugins = [require('autoprefixer')(), require('tailwindcss')()];
 
-if (!isH5) {
+if (!WeappTailwindcssDisabled) {
   plugins.push(
     require('postcss-rem-to-responsive-pixel')({
       rootValue: 32,
       propList: ['*'],
-      transformUnit: 'rpx'
+      transformUnit: 'rpx',
     })
   );
 
-  plugins.push(require('weapp-tailwindcss-webpack-plugin/postcss')());
+  // plugins.push(require('weapp-tailwindcss-webpack-plugin/postcss')());
 }
 
 module.exports = {
-  plugins
+  plugins,
 };
