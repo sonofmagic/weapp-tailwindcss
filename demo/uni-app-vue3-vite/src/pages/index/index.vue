b7676a50af6c307e3c58a30bdd926f960f6465ad
SonOfMagic <qq1324318532@gmail.com>
2022-10-28 13:58:43 +0800

    chore: border-b-[4rpx] become border-bottom-color

--- demo/uni-app-vue3-vite/src/pages/index/index.vue
+++ demo/uni-app-vue3-vite/src/pages/index/index.vue
@@ -1,5 +1,6 @@
 <template>
   <view class="content">
+    <view class="border-b-[4rpx] border-t-[4px] border w-screen h-3 border-[red]"></view>
     <view class="[&_.u-count-down\_\_text]:!text-red-400">
       <view></view>
       <view></view>
