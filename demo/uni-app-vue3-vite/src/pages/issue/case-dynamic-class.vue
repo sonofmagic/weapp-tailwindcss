440339129a153fed1634bb32894c837b754add70
SonOfMagic <qq1324318532@gmail.com>
2022-10-31 15:35:17 +0800

    feat: commit bug cases

---
+++ demo/uni-app-vue3-vite/src/pages/issue/case-dynamic-class.vue
@@ -0,0 +1,21 @@
+<template>
+  <view>
+    <view :key="idx" v-for="(item,idx) in items">
+      <view :class="[item.disabled ? 'w-[52px] h-[52px] block bg-[#123456]' : '']"></view>
+    </view>
+  </view>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+const items = ref(new Array(10).fill(0).map((x, idx) => {
+  return {
+    disabled: idx % 2 === 0
+  }
+}))
+
+</script>
+
+<style scoped>
+
+</style>
