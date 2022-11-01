2540b4c43b1b8512a1a6ba47ef59fa7596270242
SonOfMagic <qq1324318532@gmail.com>
2022-11-01 15:37:28 +0800

    feat: commit if compile

--- demo/uni-app-vue3-vite/src/pages/index/index.vue
+++ demo/uni-app-vue3-vite/src/pages/index/index.vue
@@ -1,5 +1,10 @@
 <template>
   <view class="content">
+
+    <view class="text-[32rpx] border-[10rpx]">rpx</view>
+    <view class="text-[32px] border-[10px]">px</view>
+
+
     <view class="!text-primary !border-primary text-[32px]">1111</view>
     <view class="border-b-[4rpx] border-t-[4px] border w-screen h-3 border-[red]"></view>
     <view class="[&_.u-count-down\_\_text]:!text-red-400">
@@ -83,11 +88,11 @@ const go2SubDemo = () => {
   });
 };
 const themeRef = ref(uni.getSystemInfoSync().theme);
-// #if mp-weixin
+// #ifdef MP
 uni.onThemeChange(({ theme }: { theme: 'dark' | 'light' }) => {
   themeRef.value = theme;
 });
-// #end if
+// #endif
 onBeforeUnmount(() => {
   uni.offThemeChange(() => { });
 });
