f85d591b3838b0dda8fb4c604e9ba0a8d0b56f26
sonofmagic <1324318532@qq.com>
2022-11-03 22:44:50 +0800

    chore: eslint extract pkg install

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,4 @@
 module.exports = {
-  root: false,
-  env: {
-    es2021: true,
-    node: true,
-    'jest/globals': true
-  },
-  extends: ['standard', 'plugin:prettier/recommended'],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaVersion: 12,
-    sourceType: 'module'
-  },
-  plugins: ['@typescript-eslint', 'jest', 'prettier'],
-  rules: {
-    'prettier/prettier': 'error'
-  },
-  globals: {}
+  root: true,
+  extends: ['@icebreakers/eslint-config-ts']
 }
