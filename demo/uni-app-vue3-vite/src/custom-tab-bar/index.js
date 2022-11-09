49705c49fb06affab8a399be39b7d80224ee1e66
SonOfMagic <qq1324318532@gmail.com>
2022-11-09 18:35:50 +0800

    feat: commit native codes

--- demo/uni-app-vue3-vite/src/custom-tab-bar/index.js
+++ demo/uni-app-vue3-vite/src/custom-tab-bar/index.js
@@ -1,35 +1,39 @@
+// import { ComponentWithComputed } from 'miniprogram-computed';
+// const computedBehavior = require('miniprogram-computed').behavior;
 const appInstance = getApp();
-
 Component({
+  behaviors: [],
   data: {
     selected: 0,
     color: '#7A7E83',
     selectedColor: '#3cc51f',
-    list: [
-      {
-        pagePath: '/pages/index/index',
-        text: '首页',
-      },
-      {
-        pagePath: '/pages/issue/case55',
-        text: 'case55',
-      },
-    ],
+    list: appInstance.globalData.list,
   },
   attached() {},
+  // computed: {
+  //   list() {
+  //     return appInstance.globalData.list;
+  //   },
+  // },
   methods: {
-    async switchTab(e) {
+    switchTab(e) {
       const data = e.currentTarget.dataset;
       const url = data.path;
-      console.log(data, appInstance.globalData);
-      await wx.switchTab({ url });
-
-      const tabBar = this.getTabBar();
-      [this, tabBar].forEach((x) => {
-        x.setData({
-          selected: data.index,
-        });
+      wx.switchTab({ url });
+      this.setData({
+        selected: data.index,
       });
+
+      // const tabBar = this.getTabBar();
+      // [this, tabBar].forEach((x) => {
+      //   x.setData({
+      //     selected: data.index,
+      //   });
+      // });
+      // appInstance.globalData.selected = data.index;
+      // tabBar.setData({
+      //   selected: data.index,
+      // });
     },
   },
 });
