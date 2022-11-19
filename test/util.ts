680c1f3ffd180f9aa87b3455e251cf7f3526b5db
sonofmagic <1324318532@qq.com>
2022-11-20 01:24:46 +0800

    feat: commit upgrade demos 2 beta

--- test/util.ts
+++ test/util.ts
@@ -6,6 +6,7 @@ export function resolve(...args: string[]) {
 }
 
 export const rootPath = resolve(__dirname, '..')
+export const demoPath = resolve(rootPath, 'demo')
 export const configPath = resolve(__dirname, 'config')
 export const jsxCasePath = resolve(__dirname, 'fixtures/jsx')
 export const cssCasePath = resolve(__dirname, 'fixtures/css')
