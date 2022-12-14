f12d97b5a1943291558f235aeaa987afab19a992
SonOfMagic <qq1324318532@gmail.com>
2022-12-14 15:06:55 +0800

    demo: commit uniapp编译为小程序后!前缀转换错误 #134 test case

---
+++ demo/uni-app/src/pages/index/issue134.vue
@@ -0,0 +1,22 @@
+<template>
+    <div :class="[{ popup: !maxFull }, className, { 'rounded-t-lg !w-full': type == 'bottom' && !className }]">
+        issue134
+    </div>
+</template>
+
+<script>
+// https://github.com/sonofmagic/weapp-tailwindcss-webpack-plugin/issues/134
+export default {
+    data() {
+        return {
+            className: '',
+            maxFull: false,
+            type: 'bottom'
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
