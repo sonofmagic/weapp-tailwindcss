90c622c30340cffcc51e858902d66b98b03f45d6
SonOfMagic <qq1324318532@gmail.com>
2023-03-14 10:06:51 +0800

    feat: commit ast patch

--- src/tailwindcss/inspector.ts
+++ src/tailwindcss/inspector.ts
@@ -1,8 +1,32 @@
 import path from 'path'
+import t from '@babel/types'
+import { parse, traverse, generate } from '@/babel'
 export function getContexts() {
   const injectFilePath = path.join(process.cwd(), 'node_modules', 'tailwindcss/lib/index.js')
   const p = require.resolve(injectFilePath)
   const mo = require(p)
   return (mo.contextRef.value as any[]).map(x => x.classCache)
   // processTailwindFeatures return content
+}
+
+export function inspectProcessTailwindFeaturesReturnContext(content: string) {
+  const ast = parse(content)
+  let start = false
+  traverse(ast, {
+    FunctionDeclaration(p) {
+      const n = p.node
+      if (n.id?.name === 'processTailwindFeatures') {
+        start = true
+      }
+    },
+    BlockStatement(p) {
+      const n = p.node
+      if (start && p.parent.type === 'FunctionExpression') {
+        const rts = t.returnStatement()
+        rts.argument = t.identifier('context')
+        n.body.push(rts)
+      }
+    },
+  })
+  return generate(ast)
 }
