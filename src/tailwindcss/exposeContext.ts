1aeff6181152dec40dee2e5ec2e0054550577a98
SonOfMagic <qq1324318532@gmail.com>
2023-03-15 10:35:47 +0800

    feat: patch and get getContexts

--- src/tailwindcss/exposeContext.ts
+++ src/tailwindcss/exposeContext.ts
@@ -2,9 +2,9 @@ import path from 'path'
 import fs from 'fs'
 
 export function getContexts() {
-  // const cwd = process.cwd()
-  // path.join(cwd, 'node_modules', 'tailwindcss/lib')
-  const distPath = 'tailwindcss/lib'
+  const cwd = process.cwd()
+  //
+  const distPath = path.join(cwd, 'node_modules', 'tailwindcss/lib')
   // index.js
   // plugin.js
   let injectFilePath = path.join(distPath, 'plugin.js')
@@ -13,6 +13,29 @@ export function getContexts() {
   }
   const p = require.resolve(injectFilePath)
   const mo = require(p)
-  return (mo.contextRef.value as any[]).map((x) => x.classCache)
+  if (mo.contextRef) {
+    return mo.contextRef.value as any[]
+  }
+  return []
+
+  // processTailwindFeatures return content
+}
+
+export function getClassCache() {
+  const cwd = process.cwd()
+  // path.join(cwd, 'node_modules', 'tailwindcss/lib')
+  const distPath = path.join(cwd, 'node_modules', 'tailwindcss/lib')
+  // index.js
+  // plugin.js
+  let injectFilePath = path.join(distPath, 'plugin.js')
+  if (!fs.existsSync(injectFilePath)) {
+    injectFilePath = path.join(distPath, 'index.js')
+  }
+  const p = require.resolve(injectFilePath)
+  const mo = require(p)
+  if (mo.contextRef) {
+    return (mo.contextRef.value as any[]).map((x) => x.classCache)
+  }
+  return []
   // processTailwindFeatures return content
 }
