6a1287055c23a1a9d2c1cdbb7bbdd477ee862ae3
SonOfMagic <qq1324318532@gmail.com>
2023-03-15 17:44:17 +0800

    feat: commit expose class set

--- src/tailwindcss/exposeContext.ts
+++ src/tailwindcss/exposeContext.ts
@@ -1,5 +1,6 @@
 import path from 'path'
 import fs from 'fs'
+import type { Rule } from 'postcss'
 
 export function getContexts() {
   const cwd = process.cwd()
@@ -17,25 +18,32 @@ export function getContexts() {
     return mo.contextRef.value as any[]
   }
   return []
-
-  // processTailwindFeatures return content
 }
 
-export function getClassCache() {
-  const cwd = process.cwd()
-  // path.join(cwd, 'node_modules', 'tailwindcss/lib')
-  const distPath = path.join(cwd, 'node_modules', 'tailwindcss/lib')
-  // index.js
-  // plugin.js
-  let injectFilePath = path.join(distPath, 'plugin.js')
-  if (!fs.existsSync(injectFilePath)) {
-    injectFilePath = path.join(distPath, 'index.js')
+export function getClassCaches(): Map<
+  string,
+  (
+    | {
+        layer: string
+        options: Record<string, any>
+        sort: Record<string, any>
+      }
+    | Rule
+  )[]
+>[] {
+  const contexts = getContexts()
+  return (contexts as any[]).map((x) => x.classCache)
+}
+
+export function getClassCacheSet(): Set<string> {
+  const classCaches = getClassCaches()
+  const classSet = new Set<string>()
+  for (let i = 0; i < classCaches.length; i++) {
+    const classCacheMap = classCaches[i]
+    const keys = classCacheMap.keys()
+    for (const key of keys) {
+      classSet.add(key)
+    }
   }
-  const p = require.resolve(injectFilePath)
-  const mo = require(p)
-  if (mo.contextRef) {
-    return (mo.contextRef.value as any[]).map((x) => x.classCache)
-  }
-  return []
-  // processTailwindFeatures return content
+  return classSet
 }
