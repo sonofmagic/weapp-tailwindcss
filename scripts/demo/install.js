93b6bd9f1e10a630d95636154d3051e3e956a16b
ice breaker <1324318532@qq.com>
2023-03-19 05:25:38 +0800

    chore: upgrade deps

--- scripts/demo/install.js
+++ scripts/demo/install.js
@@ -1,16 +1,16 @@
 const path = require('path')
-const { install } = require('@icebreakers/cli')
+const { install, raw } = require('@icebreakers/cli')
 const argvs = process.argv.slice(2)
 const isBeta = argvs.indexOf('--beta') > -1
 
 ;(async () => {
   const demoPath = path.resolve(__dirname, '../../demo')
-  await install(demoPath, undefined, true)
+  await raw(demoPath, '--ignore-engines', true)
   await install(
     demoPath,
     `-D weapp-tailwindcss-webpack-plugin${
       isBeta ? '@beta' : ''
-    } tailwindcss-rem2px-preset@latest postcss-rem-to-responsive-pixel@latest weapp-ide-cli@latest postcss-rpx-transform weapp-tailwindcss-children tailwind-css-variables-theme-generator`,
+    } tailwindcss-rem2px-preset@latest postcss-rem-to-responsive-pixel@latest weapp-ide-cli@latest postcss-rpx-transform weapp-tailwindcss-children tailwind-css-variables-theme-generator --ignore-engines`,
     true
   )
 
