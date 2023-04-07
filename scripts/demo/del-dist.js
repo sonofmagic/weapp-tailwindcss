949c7865fcd7ab43bce3805f6a2f9868668de540
SonOfMagic <qq1324318532@gmail.com>
2023-04-07 17:06:36 +0800

    chore: ensureDir and e2e test in vite project

--- scripts/demo/del-dist.js
+++ scripts/demo/del-dist.js
@@ -8,7 +8,9 @@ async function main() {
   let dirCount = 0
   for (let i = 0; i < demoPaths.length; i++) {
     const demoPath = demoPaths[i]
-    const res = await del(path.resolve(root, `demo/${demoPath}/weapp-tw-dist`))
+    const res = await del(path.resolve(root, `demo/${demoPath}/weapp-tw-dist`), {
+      force: true
+    })
     res.length && dirCount++
   }
   const res = await del(sourcePath)
