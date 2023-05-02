93a3e912f24a214570d03b0d96504fb48a06d52d
sonofmagic <1324318532@qq.com>
2023-05-02 15:57:55 +0800

    chore(demos): upgrade deps

--- demo/gulp-app/src/pages/index/index.ts
+++ demo/gulp-app/src/pages/index/index.ts
@@ -1,11 +1,13 @@
 // index.js
 // 获取应用实例
 // const app = getApp()
+
 Page({
   data: {
     motto: 'Hello World',
     userInfo: {},
-    contentStyle: 'mt-[24px]'
+    contentStyle: 'mt-[24px]',
+    classNames: "bg-[url('https://xxx.com/xx.webp')] text-[#123456] text-[50px] bg-[#fff]"
   },
   onLoad: function () {},
   copyText: function (e: any) {
