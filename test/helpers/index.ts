1a8d3cadee16cd7bd1fa4afd756fde32409c0a47
SonOfMagic <qq1324318532@gmail.com>
2023-05-12 14:02:27 +0800

    chore: remove webpack4 deps

--- test/helpers/index.ts
+++ test/helpers/index.ts
@@ -1,9 +1,9 @@
 import execute from './execute'
-import { getCompiler5, getCompiler4 } from './getCompiler'
+import { getCompiler5 } from './getCompiler'
 import compile from './compile'
 import createLoader from './createLoader'
 import readAsset from './readAsset'
 import readAssets from './readAssets'
 import getErrors from './getErrors'
 import getWarnings from './getWarnings'
-export { execute, getCompiler5, getCompiler4, compile, createLoader, readAsset, readAssets, getErrors, getWarnings }
+export { execute, getCompiler5, compile, createLoader, readAsset, readAssets, getErrors, getWarnings }
