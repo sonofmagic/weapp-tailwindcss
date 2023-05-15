6dc22aefe731c47a61b591587c27bbf04ca17d66
SonOfMagic <qq1324318532@gmail.com>
2023-05-15 18:02:59 +0800

    chore: prepare for wxmlHandler

--- src/mangle/store.ts
+++ src/mangle/store.ts
@@ -1,22 +1,32 @@
 import { ClassGenerator, cssHandler, jsHandler } from 'tailwindcss-mangle-core'
 import { UserDefinedOptions } from '@/types'
+import { splitCode } from '@/extractors/split'
+
+function getSelf(x: string) {
+  return x
+}
 // import type { BabelFileResult } from '@babel/core'
-const scope: {
+// 为什么要用这种方式，因为一层一层往下传递参数，太烦了
+// 所以说还是搞个 IOC 容器比较爽，要什么注入什么
+export const scope: {
   rawOptions: UserDefinedOptions['mangle']
-  runtimeSet?: Set<string>
+  runtimeSet: Set<string>
   classGenerator: ClassGenerator
   cssHandler: (rawSource: string) => string //  typeof cssHandler
   jsHandler: (rawSource: string) => string // typeof jsHandler
+  wxmlHandler: (rawSource: string) => string
 } = {
   rawOptions: false,
+  runtimeSet: new Set<string>(),
   classGenerator: new ClassGenerator(),
-  cssHandler: (x) => x,
-  jsHandler: (x) => x
-}
+  cssHandler: getSelf,
+  jsHandler: getSelf,
+  wxmlHandler: getSelf
+} // as default
 
-export function useStore() {
-  return scope as Required<typeof scope>
-}
+// export function useStore() {
+//   return scope as Required<typeof scope>
+// }
 
 export function initStore(options: UserDefinedOptions['mangle']) {
   scope.rawOptions = options
@@ -41,6 +51,11 @@ export function initStore(options: UserDefinedOptions['mangle']) {
         runtimeSet: scope.runtimeSet!,
         scene: 'process'
       })
+    }
+
+    scope.wxmlHandler = (rawSource: string) => {
+      //  splitCode(rawSource)
+      return rawSource
     }
   }
 }
