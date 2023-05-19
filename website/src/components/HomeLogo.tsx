629ec9b90f87be4b72360055ac8a83a4089b8e3c
sonofmagic <1324318532@qq.com>
2023-05-19 21:55:47 +0800

    chore: use svg for index page

--- website/src/components/HomeLogo.tsx
+++ website/src/components/HomeLogo.tsx
@@ -1,13 +1,14 @@
 import React from 'react'
 // import WeappIcon from '@site/src/assets/weapp.svg'
 // import TwLogoIcon from '@site/src/assets/tw-logo.svg'
-import TwLogoIcon from '@site/static/img/logo.png'
+// import TwLogoIcon from '@site/static/img/logo.png'
 // import LinkIcon from '@site/src/assets/link.svg'
-
+import TwLogoIcon from '@site/../assets/logo.svg'
 export default function HomeLogo() {
   return (
     <div className="flex items-center justify-center">
-      <img src={TwLogoIcon} className="dark:text-white" alt="weapp-tailwindcss"></img>
+      <TwLogoIcon></TwLogoIcon>
+      {/* <img src={TwLogoIcon} className="dark:text-white" alt="weapp-tailwindcss"></img> */}
       {/* <TwLogoIcon className="dark:text-white"></TwLogoIcon>
       <LinkIcon className="w-12 mx-4 dark:text-white"></LinkIcon>
       <WeappIcon className="w-24"></WeappIcon> */}
