ec09454090dcf8787bdda4ed033e1f487d19d8bf
SonOfMagic <qq1324318532@gmail.com>
2023-05-24 14:47:54 +0800

    test: add customTempleteHandler unit test

---
+++ test/wxml/customTempleteHandler.test.ts
@@ -0,0 +1,13 @@
+import { customTempleteHandler } from '@/wxml/utils'
+import { SimpleMappingChars2String } from '@/dic'
+
+describe('customTempleteHandler', () => {
+  it('invalid customAttributesEntities options', () => {
+    const res = customTempleteHandler('<view class="p-[20px]"></view>', {
+      // @ts-ignore
+      customAttributesEntities: {},
+      escapeMap: SimpleMappingChars2String
+    })
+    expect(res).toBe('<view class="p-_20px_"></view>')
+  });
+});
