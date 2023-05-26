890b113fadd9e212b0e54be295637b11dfea4d08
SonOfMagic <qq1324318532@gmail.com>
2023-05-26 16:48:04 +0800

    fix(action): use peaceiris/actions-gh-pages

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -36,7 +36,7 @@ const config = {
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: isGithub ? '/weapp-tailwindcss/' : '/',
-
+  trailingSlash: false,
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'sonofmagic', // Usually your GitHub org/user name.
