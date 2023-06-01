bdbee15ef2b24657545ca9c10b428442bb7eb94d
sonofmagic <1324318532@qq.com>
2023-06-01 23:00:23 +0800

    test: add empty webpack case

--- test/webpack.v5.test.ts
+++ test/webpack.v5.test.ts
@@ -1,131 +1,92 @@
 import path from 'node:path'
 import fs from 'node:fs/promises'
-import type { Compiler } from 'webpack'
+import type { Compiler, Configuration } from 'webpack'
 import postcss from 'postcss'
 import { getCompiler5, compile, readAssets, createLoader, getErrors, getWarnings } from './helpers'
 import { UnifiedWebpackPluginV5 } from '@/index'
 
+function createCompiler(params: Pick<Configuration, 'mode' | 'entry'> & { tailwindcssConfig: string }) {
+  const { entry, mode, tailwindcssConfig } = params
+
+  const processor = postcss([
+    // require('autoprefixer')(),
+    require('tailwindcss')({ config: tailwindcssConfig })
+    // require('postcss-rem-to-responsive-pixel')({
+    //   rootValue: 32,
+    //   propList: ['*'],
+    //   transformUnit: 'rpx'
+    // })
+  ])
+
+  return getCompiler5({
+    mode,
+    entry,
+    output: {
+      path: path.resolve(__dirname, './dist'),
+      filename: '[name].js', // ?var=[fullhash]
+      chunkFilename: '[id].[name].js' // ?ver=[fullhash]
+    },
+    module: {
+      rules: [
+        {
+          test: /\.m?js$/,
+          // https://webpack.js.org/configuration/module/#useentry
+          use: createLoader(async function (source) {
+            const basename = path.basename(this.resourcePath, path.extname(this.resourcePath))
+            const filename = basename + '.wxml'
+            const content = await fs.readFile(path.resolve(this.context, filename), {
+              encoding: 'utf8'
+            })
+            this.emitFile(filename, content)
+
+            const cssFilename = basename + '.css'
+
+            const cssContent = await fs.readFile(path.resolve(this.context, cssFilename), {
+              encoding: 'utf8'
+            })
+
+            const res = await processor.process(cssContent, {
+              from: undefined,
+              map: false
+            })
+            this.emitFile(cssFilename, res.toString())
+            return source
+            // this.emitFile('hello.xx', '12345')
+            // console.log()
+          })
+        }
+
+      ]
+    }
+  })
+}
 describe('webpack5 plugin', () => {
   let compiler: Compiler
   let prodCompiler: Compiler
+  let emptyCompiler: Compiler
   beforeEach(() => {
-    const processor = postcss([
-      // require('autoprefixer')(),
-      require('tailwindcss')({ config: path.resolve(__dirname, './fixtures/webpack/v5/wxml/tailwind.config.js') })
-      // require('postcss-rem-to-responsive-pixel')({
-      //   rootValue: 32,
-      //   propList: ['*'],
-      //   transformUnit: 'rpx'
-      // })
-    ])
-    compiler = getCompiler5({
+    compiler = createCompiler({
       mode: 'development',
       entry: {
         wxml: path.resolve(__dirname, './fixtures/webpack/v5/wxml/pages/index.js')
       },
-      output: {
-        path: path.resolve(__dirname, './dist'),
-        filename: '[name].js', // ?var=[fullhash]
-        chunkFilename: '[id].[name].js' // ?ver=[fullhash]
-      },
-      module: {
-        rules: [
-          {
-            test: /\.m?js$/,
-            // https://webpack.js.org/configuration/module/#useentry
-            use: createLoader(async function (source) {
-              const basename = path.basename(this.resourcePath, path.extname(this.resourcePath))
-              const filename = basename + '.wxml'
-              const content = await fs.readFile(path.resolve(this.context, filename), {
-                encoding: 'utf8'
-              })
-              this.emitFile(filename, content)
-
-              const cssFilename = basename + '.css'
-
-              const cssContent = await fs.readFile(path.resolve(this.context, cssFilename), {
-                encoding: 'utf8'
-              })
-
-              const res = await processor.process(cssContent, {
-                from: undefined,
-                map: false
-              })
-              this.emitFile(cssFilename, res.toString())
-              return source
-              // this.emitFile('hello.xx', '12345')
-              // console.log()
-            })
-          }
-          // {
-          //   test: /\.css$/,
-          //   use: [
-          //     createLoader(function (source) {
-          //       const basename = path.basename(this.resourcePath, path.extname(this.resourcePath))
-          //       const filename = basename + '.css'
-          //       this.emitFile(filename, source)
-          //       return ''
-          //     }),
-          //     {
-          //       loader: 'postcss-loader',
-          //       options: {
-          //         postcssOptions: {
-          //           plugins: [require('tailwindcss')({ config: path.resolve(__dirname, './fixtures/webpack/v5/wxml/tailwind.config.js') })]
-          //         }
-          //       }
-          //     }
-          //   ]
-          // },
-          // {
-          //   test: /\.wxml$/,
-          //   use: createLoader(function (source) {
-          //     const basename = path.basename(this.resourcePath, path.extname(this.resourcePath))
-          //     const filename = basename + '.wxml'
-          //     this.emitFile(filename, source)
-          //   })
-          // }
-        ]
-      }
+      tailwindcssConfig: path.resolve(__dirname, './fixtures/webpack/v5/wxml/tailwind.config.js')
     })
 
-    prodCompiler = getCompiler5({
+    prodCompiler = createCompiler({
       mode: 'production',
       entry: {
         wxml: path.resolve(__dirname, './fixtures/webpack/v5/wxml/pages/index.js')
       },
-      output: {
-        path: path.resolve(__dirname, './dist'),
-        filename: '[name].js', // ?var=[fullhash]
-        chunkFilename: '[id].[name].js' // ?ver=[fullhash]
+      tailwindcssConfig: path.resolve(__dirname, './fixtures/webpack/v5/wxml/tailwind.config.js')
+    })
+
+    emptyCompiler = createCompiler({
+      mode: 'production',
+      entry: {
+        wxml: path.resolve(__dirname, './fixtures/webpack/v5/empty/index.js')
       },
-      module: {
-        rules: [
-          {
-            test: /\.m?js$/,
-            use: createLoader(async function (source) {
-              const basename = path.basename(this.resourcePath, path.extname(this.resourcePath))
-              const filename = basename + '.wxml'
-              const content = await fs.readFile(path.resolve(this.context, filename), {
-                encoding: 'utf8'
-              })
-              this.emitFile(filename, content)
-
-              const cssFilename = basename + '.css'
-
-              const cssContent = await fs.readFile(path.resolve(this.context, cssFilename), {
-                encoding: 'utf8'
-              })
-
-              const res = await processor.process(cssContent, {
-                from: undefined,
-                map: false
-              })
-              this.emitFile(cssFilename, res.toString())
-              return source
-            })
-          }
-        ]
-      }
+      tailwindcssConfig: path.resolve(__dirname, './fixtures/webpack/v5/empty/tailwind.config.js')
     })
   })
   it('common', async () => {
@@ -142,6 +103,20 @@ describe('webpack5 plugin', () => {
     expect(getErrors(stats)).toMatchSnapshot('errors')
     expect(getWarnings(stats)).toMatchSnapshot('warnings')
   })
+
+  it('empty build', async () => {
+    new UnifiedWebpackPluginV5({
+      mainCssChunkMatcher(name) {
+        return path.basename(name) === 'index.css'
+      },
+    }).apply(emptyCompiler)
+
+    const stats = await compile(emptyCompiler)
+
+    expect(readAssets(emptyCompiler, stats)).toMatchSnapshot('assets')
+    expect(getErrors(stats)).toMatchSnapshot('errors')
+    expect(getWarnings(stats)).toMatchSnapshot('warnings')
+  });
 
   it('unified common', async () => {
     new UnifiedWebpackPluginV5({
