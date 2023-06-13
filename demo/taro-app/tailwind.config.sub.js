51ac10eef4c2eb61d0d1b9858a58020caa17329c
SonOfMagic <qq1324318532@gmail.com>
2023-06-13 17:36:36 +0800

    chore: commit two part tailwindcss config

--- demo/taro-app/tailwind.config.sub.js
+++ demo/taro-app/tailwind.config.sub.js
@@ -1,7 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  // https://github.com/mrmlnc/fast-glob
-  content: ["./src/**/*.{html,js,ts,jsx,tsx}", "!./src/moduleB/**/*.{html,js,ts,jsx,tsx}"],
+  content: ["./src/moduleB/**/*.{html,js,ts,jsx,tsx}"],
   darkMode: 'class',
   theme: {
     extend: {},
