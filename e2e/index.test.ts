91b1634ded43fe3a73d51008e63556866e9f75f6
ice breaker <1324318532@qq.com>
2023-06-15 00:09:16 +0800

    chore: e2e test snap update

--- e2e/index.test.ts
+++ e2e/index.test.ts
@@ -10,58 +10,58 @@ const TestProjectsEntries: {
   testMethod: Function
   url?: string
 }[] = [
-    {
-      name: 'uni-app',
-      projectPath: 'uni-app/dist/build/mp-weixin',
-      testMethod: () => {}
-    },
-    {
-      name: 'uni-app-vue2-cli5',
-      projectPath: 'uni-app-webpack5/dist/build/mp-weixin',
-      testMethod: () => {}
-    },
-    {
-      name: 'uni-app-vue3-vite',
-      projectPath: 'uni-app-vue3-vite/dist/build/mp-weixin',
-      testMethod: () => {}
-    },
-    {
-      name: 'taro-react',
-      projectPath: 'taro-app',
-      testMethod: () => {}
-    },
-    {
-      name: 'taro-vue3',
-      projectPath: 'taro-vue3-app',
-      testMethod: () => {}
-    },
-    {
-      name: 'taro-vue2',
-      projectPath: 'taro-vue2-app',
-      testMethod: () => {}
-    },
-    {
-      name: 'gulp-app',
-      projectPath: 'gulp-app',
-      testMethod: () => {}
-    },
-    {
-      name: 'mpx-app',
-      projectPath: 'mpx-app/dist/wx',
-      testMethod: () => {},
-      url: '/pages/index'
-    },
-    {
-      name: 'native-mina',
-      projectPath: 'native-mina',
-      testMethod: () => {}
-    },
-    {
-      name: 'rax-app',
-      projectPath: 'rax-app/build/wechat-miniprogram',
-      testMethod: () => {}
-    }
-  ]
+  {
+    name: 'uni-app',
+    projectPath: 'uni-app/dist/build/mp-weixin',
+    testMethod: () => {}
+  },
+  {
+    name: 'uni-app-vue2-cli5',
+    projectPath: 'uni-app-webpack5/dist/build/mp-weixin',
+    testMethod: () => {}
+  },
+  {
+    name: 'uni-app-vue3-vite',
+    projectPath: 'uni-app-vue3-vite/dist/build/mp-weixin',
+    testMethod: () => {}
+  },
+  {
+    name: 'taro-react',
+    projectPath: 'taro-app',
+    testMethod: () => {}
+  },
+  {
+    name: 'taro-vue3',
+    projectPath: 'taro-vue3-app',
+    testMethod: () => {}
+  },
+  {
+    name: 'taro-vue2',
+    projectPath: 'taro-vue2-app',
+    testMethod: () => {}
+  },
+  {
+    name: 'gulp-app',
+    projectPath: 'gulp-app',
+    testMethod: () => {}
+  },
+  {
+    name: 'mpx-app',
+    projectPath: 'mpx-app/dist/wx',
+    testMethod: () => {},
+    url: '/pages/index'
+  },
+  {
+    name: 'native-mina',
+    projectPath: 'native-mina',
+    testMethod: () => {}
+  },
+  {
+    name: 'rax-app',
+    projectPath: 'rax-app/build/wechat-miniprogram',
+    testMethod: () => {}
+  }
+]
 
 function wait(ts = 1000) {
   return new Promise((resolve) => {
@@ -90,5 +90,5 @@ describe('e2e', () => {
 
     await miniProgram.close()
     await wait()
-  });
+  })
 })
