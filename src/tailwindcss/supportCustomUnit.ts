8931119d9dc3bd7c73ca396b793351dff07e38d7
ice breaker <1324318532@qq.com>
2023-06-25 19:28:14 +0800

    chore: format file content

--- src/tailwindcss/supportCustomUnit.ts
+++ src/tailwindcss/supportCustomUnit.ts
@@ -12,28 +12,28 @@ export function findAstNode(content: string, options: ILengthUnitsPatchOptions) 
   traverse(ast, {
     Identifier(path) {
       if (path.node.name === DOPTS.variableName && t.isVariableDeclarator(path.parent) && t.isArrayExpression(path.parent.init)) {
-            arrayRef = path.parent.init
-            const set = new Set(path.parent.init.elements.map((x) => (<StringLiteral>x).value))
-            for (let i = 0; i < options.units.length; i++) {
-              const unit = options.units[i]
-              if (!set.has(unit)) {
-                path.parent.init.elements = path.parent.init.elements.map((x) => {
-                  if (t.isStringLiteral(x)) {
-                    return {
-                      type: x?.type,
-                      value: x?.value
-                    }
-                  }
-                  return x
-                })
-                path.parent.init.elements.push({
-                  type: 'StringLiteral',
-                  value: unit
-                })
-                changed = true
+        arrayRef = path.parent.init
+        const set = new Set(path.parent.init.elements.map((x) => (<StringLiteral>x).value))
+        for (let i = 0; i < options.units.length; i++) {
+          const unit = options.units[i]
+          if (!set.has(unit)) {
+            path.parent.init.elements = path.parent.init.elements.map((x) => {
+              if (t.isStringLiteral(x)) {
+                return {
+                  type: x?.type,
+                  value: x?.value
+                }
               }
-            }
+              return x
+            })
+            path.parent.init.elements.push({
+              type: 'StringLiteral',
+              value: unit
+            })
+            changed = true
           }
+        }
+      }
     }
   })
   return {
