1153a3cf1522612b07b6c6c5e07e3eba177fa8d8
ice breaker <1324318532@qq.com>
2023-07-19 20:12:57 +0800

    chore: get decl script update

--- scripts/get-decl.ts
+++ scripts/get-decl.ts
@@ -1,11 +1,29 @@
 import fs from 'node:fs'
 import path from 'node:path'
 import postcss, { Declaration, Rule } from 'postcss'
+import tailwindcss from 'tailwindcss'
+async function main() {
+  const source = await postcss([
+    tailwindcss({
+      content: {
+        files: [
+          {
+            raw: ''
+          }
+        ]
+      },
+      corePlugins: {
+        preflight: false
+      }
+    })
+  ]).process(`@tailwind base;
+  @tailwind components;
+  @tailwind utilities;`)
+  fs.writeFileSync(path.resolve(__dirname, 'var.css'), source.css, 'utf8')
 
-function main() {
-  const rawSource = path.resolve(__dirname, '../test/fixtures/css/css-var-scope-raw.css')
+  // const rawSource = path.resolve(__dirname, '../test/fixtures/css/css-var-scope-raw.css')
   const target = path.resolve(__dirname, '../src/postcss/cssVars.ts')
-  const content = fs.readFileSync(rawSource, 'utf8')
+  const content = source.css // fs.readFileSync(rawSource, 'utf8')
 
   const root = postcss.parse(content)
   const rule = root.nodes[0] as Rule
