d079f1374ca7b04833786124d6335a05ec5fa9a0
ice breaker <1324318532@qq.com>
2023-07-21 19:13:39 +0800

    chore: upgrade deps

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,13 +1,5 @@
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
   root: true,
-  extends: ['icebreaker', 'plugin:prettier/recommended'],
-  rules: {
-    'unicorn/no-array-reduce': 0,
-    'unicorn/no-object-as-default-parameter': 0,
-    'unicorn/filename-case': 0,
-    'unicorn/no-null': 0,
-    'unicorn/prefer-module': 0,
-    'unicorn/prefer-top-level-await': 0
-  }
+  extends: ['icebreaker', 'plugin:prettier/recommended']
 }
