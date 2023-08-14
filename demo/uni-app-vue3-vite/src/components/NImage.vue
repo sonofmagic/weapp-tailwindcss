7907fe6d749b7008b92ac6618c874f052ea56572
SonOfMagic <qq1324318532@gmail.com>
2023-08-14 17:32:21 +0800

    chore: add image coms

---
+++ demo/uni-app-vue3-vite/src/components/NImage.vue
@@ -0,0 +1,15 @@
+<template>
+  <view>
+    <image src="https://pic1.zhimg.com/v2-3ee20468f54bbfefcd0027283b21aaa8_720w.jpg"></image>
+  </view>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+
+})
+</script>
+
+<style scoped></style>
