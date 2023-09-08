a6d61e06698fdd44f104ef3af5f94689234abdc6
SonOfMagic <qq1324318532@gmail.com>
2023-09-08 23:11:51 +0800

    fix: #228 add virtualHostClass support

---
+++ demo/uni-app-vue3-vite/src/components/virtualHost/A.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="aaa">
+    <slot></slot>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  options: {
+    virtualHost: true,
+  },
+  props: {
+
+  },
+  setup() {
+    return {}
+  }
+})
+</script>
+
+<style scoped></style>
