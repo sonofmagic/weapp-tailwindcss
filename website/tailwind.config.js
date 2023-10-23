6fda01fac138fcb27996ab854439d3dc19cba2e3
ice breaker <1324318532@qq.com>
2023-10-23 21:34:29 +0800

    chore: add beian info link

--- website/tailwind.config.js
+++ website/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  content: ['./src/**/*.{js,jsx,ts,tsx}', './docusaurus.config.js'],
   darkMode: ['class', '[data-theme="dark"]'],
   theme: {
     extend: {}
