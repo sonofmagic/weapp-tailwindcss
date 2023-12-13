883969ae16b4460f163bbad4f250f28ef49bcf0f
ice breaker <1324318532@qq.com>
2023-12-13 16:51:42 +0800

    chore: add nodejs version check

--- scripts/raw/index.ts
+++ scripts/raw/index.ts
@@ -1,3 +1,3 @@
-async function main () {}
+async function main() {}
 
 main()
--- src/cli.ts
+++ src/cli.ts
@@ -1,10 +1,17 @@
+import semver from 'semver'
 import { createPatch } from '@/tailwindcss/patcher'
 import { getOptions } from '@/options'
 const args = process.argv.slice(2)
+const WEAPP_TW_REQUIRED_NODE_VERSION = '16.6.0'
+
+if (semver.lt(process.versions.node, WEAPP_TW_REQUIRED_NODE_VERSION)) {
+  console.error(`You are using Node.js ${process.versions.node}. For weapp-tailwindcss, Node.js version >= v${WEAPP_TW_REQUIRED_NODE_VERSION} is required.`)
+  // eslint-disable-next-line unicorn/no-process-exit
+  process.exit(1)
+}
 
 if (args[0] === 'patch') {
   const options = getOptions()
-
   const patch = createPatch(options.supportCustomLengthUnitsPatch)
 
   patch()
