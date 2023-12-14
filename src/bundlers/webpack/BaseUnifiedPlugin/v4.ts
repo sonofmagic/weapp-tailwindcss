4cfc27e3636564e524b055fbcb1e10dbc53416f9
ice breaker <1324318532@qq.com>
2023-12-14 23:55:53 +0800

    v3.0.4-webpack4.1

--- src/bundlers/webpack/BaseUnifiedPlugin/v4.ts
+++ src/bundlers/webpack/BaseUnifiedPlugin/v4.ts
@@ -74,9 +74,6 @@ export class UnifiedWebpackPluginV4 implements IBaseWebpackPlugin {
       type: null
     }
 
-    // https://github.com/dcloudio/uni-app/blob/dev/packages/webpack-uni-mp-loader/lib/plugin/index-new.js
-    // PROCESS_ASSETS_STAGE_ADDITIONAL
-    // https://github.com/NervJS/taro/blob/next/packages/taro-webpack5-runner/src/webpack/MiniWebpackPlugin.ts
     compiler.hooks.compilation.tap(pluginName, (compilation) => {
       compilation.hooks.normalModuleLoader.tap(pluginName, (loaderContext, module) => {
         if (isExisted) {
@@ -87,160 +84,160 @@ export class UnifiedWebpackPluginV4 implements IBaseWebpackPlugin {
           }
         }
       })
-
-      compiler.hooks.emit.tapPromise(
-        {
-          name: pluginName
-        },
-        async (assets) => {
-          onStart()
-          debug('start')
-
-          // 第一次进来的时候为 init
-          for (const chunk of compilation.chunks) {
-            if (chunk.id && chunk.hash) {
-              cache.calcHashValueChanged(chunk.id, chunk.hash)
-            }
-          }
-
-          const entries = Object.entries(assets)
-          const groupedEntries = getGroupedEntries(entries, this.options)
-          // 再次 build 不转化的原因是此时 set.size 为0
-          // 也就是说当开启缓存的时候没有触发 postcss,导致 tailwindcss 并没有触发
-          const runtimeSet = getClassSet()
-          setMangleRuntimeSet(runtimeSet)
-          debug('get runtimeSet, class count: %d', runtimeSet.size)
-
-          if (Array.isArray(groupedEntries.html)) {
-            let noCachedCount = 0
-            for (let i = 0; i < groupedEntries.html.length; i++) {
-              const [file, originalSource] = groupedEntries.html[i]
-
-              const rawSource = originalSource.source().toString()
-
-              const hash = cache.computeHash(rawSource)
-              const cacheKey = file
-              cache.calcHashValueChanged(cacheKey, hash)
-              await cache.process(
-                cacheKey,
-                () => {
-                  const source = cache.get(cacheKey)
-                  if (source) {
-                    compilation.updateAsset(file, source)
-                    debug('html cache hit: %s', file)
-                  } else {
-                    return false
-                  }
-                },
-                () => {
-                  const wxml = templateHandler(rawSource, {
-                    runtimeSet
-                  })
-                  const source = new ConcatSource(wxml)
-                  compilation.updateAsset(file, source)
-
-                  onUpdate(file, rawSource, wxml)
-                  debug('html handle: %s', file)
-                  noCachedCount++
-                  return {
-                    key: cacheKey,
-                    source
-                  }
-                }
-              )
-            }
-            debug('html handle finish, total: %d, no-cached: %d', groupedEntries.html.length, noCachedCount)
-          }
-
-          if (Array.isArray(groupedEntries.js)) {
-            let noCachedCount = 0
-            for (let i = 0; i < groupedEntries.js.length; i++) {
-              const [file, originalSource] = groupedEntries.js[i]
-              const cacheKey = removeExt(file)
-
-              await cache.process(
-                cacheKey,
-                () => {
-                  const source = cache.get(cacheKey)
-                  if (source) {
-                    compilation.updateAsset(file, source)
-                    debug('js cache hit: %s', file)
-                  } else {
-                    return false
-                  }
-                },
-                () => {
-                  const rawSource = originalSource.source().toString()
-                  const mapFilename = file + '.map'
-                  const hasMap = Boolean(assets[mapFilename])
-                  const { code, map } = jsHandler(rawSource, runtimeSet, {
-                    generateMap: hasMap
-                  })
-                  const source = new ConcatSource(code)
-                  compilation.updateAsset(file, source)
-                  onUpdate(file, rawSource, code)
-                  debug('js handle: %s', file)
-                  noCachedCount++
-
-                  if (hasMap && map) {
-                    const source = new RawSource(map.toString())
-                    compilation.updateAsset(mapFilename, source)
-                  }
-                  return {
-                    key: cacheKey,
-                    source
-                  }
-                }
-              )
-            }
-            debug('js handle finish, total: %d, no-cached: %d', groupedEntries.js.length, noCachedCount)
-          }
-
-          if (Array.isArray(groupedEntries.css)) {
-            let noCachedCount = 0
-            for (let i = 0; i < groupedEntries.css.length; i++) {
-              const [file, originalSource] = groupedEntries.css[i]
-
-              const rawSource = originalSource.source().toString()
-              const hash = cache.computeHash(rawSource)
-              const cacheKey = file
-              cache.calcHashValueChanged(cacheKey, hash)
-
-              await cache.process(
-                cacheKey,
-                () => {
-                  const source = cache.get(cacheKey)
-                  if (source) {
-                    compilation.updateAsset(file, source)
-                    debug('css cache hit: %s', file)
-                  } else {
-                    return false
-                  }
-                },
-                async () => {
-                  const css = await styleHandler(rawSource, {
-                    isMainChunk: mainCssChunkMatcher(file, this.appType)
-                  })
-                  const source = new ConcatSource(css)
-                  compilation.updateAsset(file, source)
-
-                  onUpdate(file, rawSource, css)
-                  debug('css handle: %s', file)
-                  noCachedCount++
-                  return {
-                    key: cacheKey,
-                    source
-                  }
-                }
-              )
-            }
-            debug('css handle finish, total: %d, no-cached: %d', groupedEntries.css.length, noCachedCount)
-          }
-
-          debug('end')
-          onEnd()
-        }
-      )
     })
+
+    compiler.hooks.emit.tapPromise(
+      {
+        name: pluginName
+      },
+      async (compilation) => {
+        onStart()
+        debug('start')
+
+        // 第一次进来的时候为 init
+        for (const chunk of compilation.chunks) {
+          if (chunk.id && chunk.hash) {
+            cache.calcHashValueChanged(chunk.id, chunk.hash)
+          }
+        }
+
+        const entries = Object.entries(compilation.assets)
+        const groupedEntries = getGroupedEntries(entries, this.options)
+        // 再次 build 不转化的原因是此时 set.size 为0
+        // 也就是说当开启缓存的时候没有触发 postcss,导致 tailwindcss 并没有触发
+        const runtimeSet = getClassSet()
+        setMangleRuntimeSet(runtimeSet)
+        debug('get runtimeSet, class count: %d', runtimeSet.size)
+
+        if (Array.isArray(groupedEntries.html)) {
+          let noCachedCount = 0
+          for (let i = 0; i < groupedEntries.html.length; i++) {
+            const [file, originalSource] = groupedEntries.html[i]
+
+            const rawSource = originalSource.source().toString()
+
+            const hash = cache.computeHash(rawSource)
+            const cacheKey = file
+            cache.calcHashValueChanged(cacheKey, hash)
+            await cache.process(
+              cacheKey,
+              () => {
+                const source = cache.get(cacheKey)
+                if (source) {
+                  compilation.updateAsset(file, source)
+                  debug('html cache hit: %s', file)
+                } else {
+                  return false
+                }
+              },
+              () => {
+                const wxml = templateHandler(rawSource, {
+                  runtimeSet
+                })
+                const source = new ConcatSource(wxml)
+                compilation.updateAsset(file, source)
+
+                onUpdate(file, rawSource, wxml)
+                debug('html handle: %s', file)
+                noCachedCount++
+                return {
+                  key: cacheKey,
+                  source
+                }
+              }
+            )
+          }
+          debug('html handle finish, total: %d, no-cached: %d', groupedEntries.html.length, noCachedCount)
+        }
+
+        if (Array.isArray(groupedEntries.js)) {
+          let noCachedCount = 0
+          for (let i = 0; i < groupedEntries.js.length; i++) {
+            const [file, originalSource] = groupedEntries.js[i]
+            const cacheKey = removeExt(file)
+
+            await cache.process(
+              cacheKey,
+              () => {
+                const source = cache.get(cacheKey)
+                if (source) {
+                  compilation.updateAsset(file, source)
+                  debug('js cache hit: %s', file)
+                } else {
+                  return false
+                }
+              },
+              () => {
+                const rawSource = originalSource.source().toString()
+                const mapFilename = file + '.map'
+                const hasMap = Boolean(assets[mapFilename])
+                const { code, map } = jsHandler(rawSource, runtimeSet, {
+                  generateMap: hasMap
+                })
+                const source = new ConcatSource(code)
+                compilation.updateAsset(file, source)
+                onUpdate(file, rawSource, code)
+                debug('js handle: %s', file)
+                noCachedCount++
+
+                if (hasMap && map) {
+                  const source = new RawSource(map.toString())
+                  compilation.updateAsset(mapFilename, source)
+                }
+                return {
+                  key: cacheKey,
+                  source
+                }
+              }
+            )
+          }
+          debug('js handle finish, total: %d, no-cached: %d', groupedEntries.js.length, noCachedCount)
+        }
+
+        if (Array.isArray(groupedEntries.css)) {
+          let noCachedCount = 0
+          for (let i = 0; i < groupedEntries.css.length; i++) {
+            const [file, originalSource] = groupedEntries.css[i]
+
+            const rawSource = originalSource.source().toString()
+            const hash = cache.computeHash(rawSource)
+            const cacheKey = file
+            cache.calcHashValueChanged(cacheKey, hash)
+
+            await cache.process(
+              cacheKey,
+              () => {
+                const source = cache.get(cacheKey)
+                if (source) {
+                  compilation.updateAsset(file, source)
+                  debug('css cache hit: %s', file)
+                } else {
+                  return false
+                }
+              },
+              async () => {
+                const css = await styleHandler(rawSource, {
+                  isMainChunk: mainCssChunkMatcher(file, this.appType)
+                })
+                const source = new ConcatSource(css)
+                compilation.updateAsset(file, source)
+
+                onUpdate(file, rawSource, css)
+                debug('css handle: %s', file)
+                noCachedCount++
+                return {
+                  key: cacheKey,
+                  source
+                }
+              }
+            )
+          }
+          debug('css handle finish, total: %d, no-cached: %d', groupedEntries.css.length, noCachedCount)
+        }
+
+        debug('end')
+        onEnd()
+      }
+    )
   }
 }
