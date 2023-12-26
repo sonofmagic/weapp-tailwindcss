ab6f7b9352937e79042d75f934b8786dc19960b0
ice breaker <1324318532@qq.com>
2023-12-26 14:45:02 +0800

    chore(demo): remove deps

--- demo/taro-app/src/pages/debug/before.tsx
+++ demo/taro-app/src/pages/debug/before.tsx
@@ -1,11 +1,11 @@
-
 import { View } from '@tarojs/components'
 
 export default function Index() {
   return (
     <View>
-      <View className='after:content-["*"] after:ml-0.5 after:text-red-500'></View>
-      <View className='after:content-[*] after:ml-0.5 after:text-red-500'></View>
+      <View className='after:content-["*"] after:ml-0.5 after:text-red-500'>111</View>
+      <View className='after:content-[*] after:ml-0.5 after:text-red-500'>111</View>
+      <View className="after:content-['*'] after:ml-0.5 after:text-red-500 bg-[#444400]">222</View>
     </View>
   )
 }
