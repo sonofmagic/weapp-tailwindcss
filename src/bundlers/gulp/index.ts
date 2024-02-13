87d79d667508054b0869705d0ae063275bca16f7
ice breaker <1324318532@qq.com>
2024-02-13 19:26:55 +0800

    v3.0.11-beta.2

--- src/bundlers/gulp/index.ts
+++ src/bundlers/gulp/index.ts
@@ -5,6 +5,8 @@ import { UserDefinedOptions } from '@/types'
 import { createTailwindcssPatcher } from '@/tailwindcss/patcher'
 import { createDebug } from '@/debug'
 
+const debug = createDebug()
+
 const Transform = stream.Transform
 
 // export interface IBaseTransformOptions {
@@ -17,7 +19,7 @@ const Transform = stream.Transform
  */
 export function createPlugins(options: UserDefinedOptions = {}) {
   const opts = getOptions(options)
-  const debug = createDebug('')
+
   const { templateHandler, styleHandler, patch, jsHandler, setMangleRuntimeSet, tailwindcssBasedir, cache } = opts
 
   let runtimeSet = new Set<string>()
@@ -26,127 +28,136 @@ export function createPlugins(options: UserDefinedOptions = {}) {
   const twPatcher = createTailwindcssPatcher()
 
   function transformWxss() {
-    const transformStream = new Transform({ objectMode: true })
+    return new Transform({
+      objectMode: true,
+      transform: async function (file: File, encoding, callback) {
+        runtimeSet = twPatcher.getClassSet({
+          basedir: tailwindcssBasedir
+        })
+        setMangleRuntimeSet(runtimeSet)
+        const error = null
 
-    transformStream._transform = async function (file: File, encoding, callback) {
-      runtimeSet = twPatcher.getClassSet({
-        basedir: tailwindcssBasedir
-      })
-      setMangleRuntimeSet(runtimeSet)
-      const error = null
+        if (file.contents) {
+          const rawSource = file.contents.toString()
+          const hash = cache.computeHash(rawSource)
+          cache.calcHashValueChanged(file.path, hash)
+          await cache.process(
+            file.path,
+            () => {
+              const source = cache.get<string>(file.path)
+              if (source) {
+                file.contents = Buffer.from(source)
+                debug('css cache hit: %s', file.path)
+              } else {
+                return false
+              }
+            },
+            async () => {
+              const code = await styleHandler(rawSource, {
+                isMainChunk: true
+              })
+              file.contents = Buffer.from(code)
+              debug('css handle: %s', file.path)
+              return {
+                key: file.path,
+                source: code
+              }
+            }
+          )
+        }
 
-      if (file.contents) {
-        const rawSource = file.contents.toString()
-        const hash = cache.computeHash(rawSource)
-        cache.calcHashValueChanged(file.path, hash)
-        await cache.process(
-          file.path,
-          () => {
-            const source = cache.get<string>(file.path)
-            if (source) {
-              file.contents = Buffer.from(source)
-              debug('css cache hit: %s', file.path)
-            } else {
-              return false
-            }
-          },
-          async () => {
-            const code = await styleHandler(rawSource, {
-              isMainChunk: true
-            })
-            file.contents = Buffer.from(code)
-            debug('css handle: %s', file.path)
-            return {
-              key: file.path,
-              source: code
-            }
-          }
-        )
+        callback(error, file)
       }
-
-      callback(error, file)
-    }
-
-    return transformStream
+      // construct(callback) {
+      //   debug('transformWxss start')
+      //   callback()
+      // }
+    })
   }
 
   function transformJs() {
-    const transformStream = new Transform({ objectMode: true })
-
-    transformStream._transform = async function (file: File, encoding, callback) {
-      const error = null
-      if (file.contents) {
-        const rawSource = file.contents.toString()
-        const hash = cache.computeHash(rawSource)
-        cache.calcHashValueChanged(file.path, hash)
-        await cache.process(
-          file.path,
-          () => {
-            const source = cache.get<string>(file.path)
-            if (source) {
-              file.contents = Buffer.from(source)
-              debug('js cache hit: %s', file.path)
-            } else {
-              return false
+    return new Transform({
+      objectMode: true,
+      transform: async function (file: File, encoding, callback) {
+        const error = null
+        if (file.contents) {
+          const rawSource = file.contents.toString()
+          const hash = cache.computeHash(rawSource)
+          cache.calcHashValueChanged(file.path, hash)
+          await cache.process(
+            file.path,
+            () => {
+              const source = cache.get<string>(file.path)
+              if (source) {
+                file.contents = Buffer.from(source)
+                debug('js cache hit: %s', file.path)
+              } else {
+                return false
+              }
+            },
+            () => {
+              const { code } = jsHandler(rawSource, runtimeSet)
+              file.contents = Buffer.from(code)
+              debug('js handle: %s', file.path)
+              return {
+                key: file.path,
+                source: code
+              }
             }
-          },
-          () => {
-            const { code } = jsHandler(rawSource, runtimeSet)
-            file.contents = Buffer.from(code)
-            debug('js handle: %s', file.path)
-            return {
-              key: file.path,
-              source: code
-            }
-          }
-        )
+          )
+        }
+        callback(error, file)
       }
-      callback(error, file)
-    }
-
-    return transformStream
+      // construct(callback) {
+      //   debug('transformJs start')
+      //   callback()
+      // }
+    })
   }
 
   function transformWxml() {
-    const transformStream = new Transform({ objectMode: true })
+    return new Transform({
+      objectMode: true,
+      transform: async function (file: File, encoding, callback) {
+        const error = null
+        // file.path
+        if (file.contents) {
+          const rawSource = file.contents.toString()
+          const hash = cache.computeHash(rawSource)
+          cache.calcHashValueChanged(file.path, hash)
 
-    transformStream._transform = async function (file: File, encoding, callback) {
-      const error = null
-      // file.path
-      if (file.contents) {
-        const rawSource = file.contents.toString()
-        const hash = cache.computeHash(rawSource)
-        cache.calcHashValueChanged(file.path, hash)
+          await cache.process(
+            file.path,
+            () => {
+              const source = cache.get<string>(file.path)
+              if (source) {
+                file.contents = Buffer.from(source)
+                debug('html cache hit: %s', file.path)
+              } else {
+                return false
+              }
+            },
+            () => {
+              const code = templateHandler(rawSource, {
+                runtimeSet
+              })
+              file.contents = Buffer.from(code)
+              debug('html handle: %s', file.path)
+              return {
+                key: file.path,
+                source: code
+              }
+            }
+          )
+        }
 
-        await cache.process(
-          file.path,
-          () => {
-            const source = cache.get<string>(file.path)
-            if (source) {
-              file.contents = Buffer.from(source)
-              debug('html cache hit: %s', file.path)
-            } else {
-              return false
-            }
-          },
-          () => {
-            const code = templateHandler(rawSource, {
-              runtimeSet
-            })
-            file.contents = Buffer.from(code)
-            debug('html handle: %s', file.path)
-            return {
-              key: file.path,
-              source: code
-            }
-          }
-        )
+        callback(error, file)
       }
-
-      callback(error, file)
-    }
-
-    return transformStream
+      // construct(callback) {
+      //   debug('transformWxml start')
+      //   callback()
+      // }
+    })
   }
 
   return {
