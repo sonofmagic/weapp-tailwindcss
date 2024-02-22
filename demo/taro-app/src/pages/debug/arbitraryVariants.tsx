0c0037bb2982368ceded3e7146ac2cb39e63a458
ice breaker <1324318532@qq.com>
2024-02-23 00:05:59 +0800

    chore(demo): add bg-gradient page demo

--- demo/taro-app/src/pages/debug/arbitraryVariants.tsx
+++ demo/taro-app/src/pages/debug/arbitraryVariants.tsx
@@ -2,15 +2,21 @@ import { View } from '@tarojs/components'
 
 const Index = () => {
   return (
-    <View className='[&_.u-count-down\_\_text]:!text-red-400'>
-      <View></View>
-      <View></View>
-      <View></View>
-      <View></View>
-      <View>
-        <View className='u-count-down__text'>u-count-down__text</View>
+    <>
+      <View className='h-14 bg-gradient-to-r from-cyan-500 to-blue-500'></View>
+      <View className='h-14 bg-gradient-to-r from-sky-500 to-indigo-500'></View>
+      <View className='h-14 bg-gradient-to-r from-violet-500 to-fuchsia-500'></View>
+      <View className='h-14 bg-gradient-to-r from-purple-500 to-pink-500'></View>
+      <View className='[&_.u-count-down\_\_text]:!text-red-400'>
+        <View></View>
+        <View></View>
+        <View></View>
+        <View></View>
+        <View>
+          <View className='u-count-down__text'>u-count-down__text</View>
+        </View>
       </View>
-    </View>
+    </>
   )
 }
 
