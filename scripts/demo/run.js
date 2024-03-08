28e7e46552798c2836b9cf78f46a565f5e8a7bf4
ice breaker <1324318532@qq.com>
2024-03-08 21:31:09 +0800

    chore: use new execaCommand

---
+++ scripts/demo/run.js
@@ -0,0 +1,49 @@
+const path = require('node:path')
+const fs = require('node:fs/promises')
+
+async function isExist(pathLike) {
+  try {
+    await fs.access(pathLike)
+    return true
+  } catch {
+    return false
+  }
+}
+
+async function setJson(p, key, flag) {
+  const json = (await isExist(p)) ? JSON.parse(await fs.readFile(p, 'utf8')) : {}
+  json[key] = flag
+  await fs.writeFile(p, JSON.stringify(json, null, 2), 'utf8')
+}
+const divideString = '-'.repeat(process.stdout.columns)
+async function run(dirPath, command) {
+  const { execaCommand } = await import('execa')
+  const filenames = await fs.readdir(dirPath)
+  for (const filename of filenames) {
+    const baseDir = path.resolve(dirPath, filename)
+    const stat = await fs.stat(baseDir)
+    if (stat.isDirectory()) {
+      const pkgPath = path.resolve(baseDir, 'package.json')
+      if (await isExist(pkgPath)) {
+        console.log(divideString)
+        console.log(`[${filename}]:${baseDir}`)
+        console.log(divideString)
+        try {
+          await execaCommand('yarn ' + command, {
+            cwd: baseDir,
+            stdio: 'inherit'
+          })
+          // .pipeStdout(process.stdout)
+          await setJson(path.resolve(dirPath, 'result.json'), filename, true)
+        } catch {
+          await setJson(path.resolve(dirPath, 'result.json'), filename, false)
+        }
+      }
+    }
+  }
+}
+
+module.exports = {
+  run,
+  isExist
+}
