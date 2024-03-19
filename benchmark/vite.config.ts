28917c464eebe6d38edd491f5820157523b32622
ice breaker <1324318532@qq.com>
2024-03-19 22:04:52 +0800

    chore: init benchmark project

---
+++ benchmark/vite.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import tailwindcss from '@tailwindcss/vite'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue(), tailwindcss()]
+})
