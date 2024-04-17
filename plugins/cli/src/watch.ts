62fe52118d0e0d32693f902f8af4dcb49b7e7953
ice breaker <1324318532@qq.com>
2024-04-17 23:14:58 +0800

    chore: use gulp instead of chokidar

--- plugins/cli/src/watch.ts
+++ plugins/cli/src/watch.ts
@@ -4,9 +4,10 @@ import type { WatchOptions } from 'chokidar'
 import defu from 'defu'
 import { createContext } from 'weapp-tailwindcss/core'
 import type { UserDefinedOptions } from 'weapp-tailwindcss'
+import gulp from 'gulp'
 import fs from 'fs-extra'
 export interface CreateWatcherOptions {
-  paths: string | readonly string[]
+  paths: string // | readonly string[]
   watchOptions: WatchOptions
   weappTailwindcssOptions: UserDefinedOptions
   outDir: string
@@ -14,39 +15,14 @@ export interface CreateWatcherOptions {
 
 export function createWatcher(options?: Partial<CreateWatcherOptions>) {
   const { paths, watchOptions, weappTailwindcssOptions, outDir } = defu<CreateWatcherOptions, Partial<CreateWatcherOptions>[]>(options, {
-    paths: '.',
+    paths: process.cwd(),
     watchOptions: {
       // persistent: false,
-      disableGlobbing: true,
+      // disableGlobbing: true,
       ignored: ['**/node_modules/**', '**/miniprogram_npm/**', '**/dist/**', /(^|[/\\])\../]
     },
     outDir: 'dist'
   })
-  const { transformJs, transformWxml, transformWxss } = createContext(weappTailwindcssOptions)
-  const watcher = chokidar.watch(paths, watchOptions)
-  chokidar.watch(paths, watchOptions).on('all', (event, path, stats) => {
-    console.log(event, path)
-    switch (event) {
-      case 'add': {
-        break
-      }
-      case 'addDir': {
-        break
-      }
-      case 'change': {
-        break
-      }
-      case 'unlink': {
-        break
-      }
-      case 'unlinkDir': {
-        break
-      }
 
-      // No default
-    }
-    // sitemap.json
-  })
-  // watcher.add('./**/*')
-  return watcher
+  const { transformJs, transformWxml, transformWxss } = createContext(weappTailwindcssOptions)
 }
--- plugins/cli/test/watch.test.ts
+++ plugins/cli/test/watch.test.ts
@@ -1,18 +1,23 @@
 import path from 'node:path'
 import fs from 'fs-extra'
+import gulp from 'gulp'
 import { fixturesPath } from './utils'
-import { createWatcher } from '@/watch'
-const stubGetWatchedCode = /getWatched\(\) {.+?return {};.+?}/s
+// import { createWatcher } from '@/watch'
+// const stubGetWatchedCode = /getWatched\(\) {.+?return {};.+?}/s
 
 describe('watch', () => {
   it('native watch', () => {
-    const watcher = createWatcher({
-      paths: path.resolve(fixturesPath, 'native')
-    })
-
-    const watchedArray = watcher.getWatched()
-    console.log(watchedArray)
-    expect(watcher.getWatched.toString()).not.toMatch(stubGetWatchedCode)
+    gulp
+      .src(['./**/*.{js,ts}', '!./dist/**/*'], {
+        cwd: path.resolve(fixturesPath, 'native')
+      })
+      .pipe(gulp.dest('dist'))
+    // const watcher = createWatcher({
+    //   paths: path.resolve(fixturesPath, 'native')
+    // })
+    // const watchedArray = watcher.getWatched()
+    // console.log(watchedArray)
+    // expect(watcher.getWatched.toString()).not.toMatch(stubGetWatchedCode)
   })
 
   it('native-ts watch', () => {})
