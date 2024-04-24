c1dbfd74bb7567bc4fffdacfb4b5e3faa58a0d43
ice breaker <1324318532@qq.com>
2024-04-24 23:46:12 +0800

    chore: release alpha version

--- apps/native-ts/.eslintrc.js
+++ apps/native-ts/.eslintrc.js
@@ -4,17 +4,18 @@
  * Install the Eslint extension before using this feature.
  */
 module.exports = {
+  root: true,
   env: {
     es6: true,
     browser: true,
-    node: true,
+    node: true
   },
-  ecmaFeatures: {
-    modules: true,
-  },
+  // ecmaFeatures: {
+  //   modules: true
+  // },
   parserOptions: {
     ecmaVersion: 2018,
-    sourceType: 'module',
+    sourceType: 'module'
   },
   globals: {
     wx: true,
@@ -24,8 +25,8 @@ module.exports = {
     getApp: true,
     Component: true,
     requirePlugin: true,
-    requireMiniProgram: true,
+    requireMiniProgram: true
   },
   // extends: 'eslint:recommended',
-  rules: {},
+  rules: {}
 }
