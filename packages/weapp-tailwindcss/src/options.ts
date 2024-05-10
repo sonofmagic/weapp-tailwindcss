cb30e458f51894a769af051c0987703f7b1db404
ice breaker <1324318532@qq.com>
2024-05-10 23:25:45 +0800

    refactor: getOptions

--- packages/weapp-tailwindcss/src/options.ts
+++ packages/weapp-tailwindcss/src/options.ts
@@ -21,12 +21,13 @@ export function getOptions(options: UserDefinedOptions = {}): InternalUserDefine
     {}
   )
 
+  result.escapeMap = result.customReplaceDictionary
+
   const {
     cssPreflight,
     customRuleCallback,
     cssPreflightRange,
     customAttributes,
-    customReplaceDictionary,
     supportCustomLengthUnitsPatch,
     arbitraryValues,
     cssChildCombinatorReplaceValue,
@@ -40,20 +41,19 @@ export function getOptions(options: UserDefinedOptions = {}): InternalUserDefine
     jsAstTool,
     babelParserOptions,
     postcssOptions,
-    cssRemoveHoverPseudoClass
+    cssRemoveHoverPseudoClass,
+    escapeMap
   } = result
 
-  result.escapeMap = customReplaceDictionary
   const cssInjectPreflight = createInjectPreflight(cssPreflight)
 
   const customAttributesEntities: ICustomAttributesEntities = isMap(options.customAttributes)
     ? [...(options.customAttributes as Exclude<ICustomAttributes, Record<string, ItemOrItemArray<string | RegExp>>>).entries()]
     : Object.entries(customAttributes)
 
-  // const custom = customAttributesEntities.length > 0
-  const { escapeMap } = result
   const { initMangle, mangleContext, setMangleRuntimeSet } = useMangleStore()
   initMangle(options.mangle)
+
   const styleHandler = createStyleHandler({
     cssInjectPreflight,
     customRuleCallback,
@@ -67,7 +67,7 @@ export function getOptions(options: UserDefinedOptions = {}): InternalUserDefine
     postcssOptions,
     cssRemoveHoverPseudoClass
   })
-  result.styleHandler = styleHandler
+
   const jsHandler = createJsHandler({
     escapeMap,
     mangleContext,
@@ -77,7 +77,6 @@ export function getOptions(options: UserDefinedOptions = {}): InternalUserDefine
     jsAstTool,
     babelParserOptions
   })
-  result.jsHandler = jsHandler
 
   const templateHandler = createTemplateHandler({
     customAttributesEntities,
@@ -87,10 +86,11 @@ export function getOptions(options: UserDefinedOptions = {}): InternalUserDefine
     jsHandler,
     disabledDefaultTemplateHandler
   })
-  result.templateHandler = templateHandler
 
+  result.styleHandler = styleHandler
+  result.jsHandler = jsHandler
+  result.templateHandler = templateHandler
   result.patch = createPatch(supportCustomLengthUnitsPatch)
-  // result.initMangle = initMangle
   result.setMangleRuntimeSet = setMangleRuntimeSet
   result.cache = cache === undefined || typeof cache === 'boolean' ? createCache(cache) : cache
 
