38675c98b1195ef81da5eb5ee4f3c46e84f02694
ice breaker <1324318532@qq.com>
2024-05-19 01:14:23 +0800

    chore(deps): upgrade

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,11 @@
 import { icebreaker } from '@icebreakers/eslint-config'
 
 export default icebreaker({}, {
-  ignores: ['**/fixtures/**', 'apps', 'demo', 'demo-linked', 'how-to-build-components-by-tailwindcss'],
+  ignores: [
+    '**/fixtures/**',
+    'apps',
+    'demo',
+    'demo-linked',
+    'how-to-build-components-by-tailwindcss',
+  ],
 })
