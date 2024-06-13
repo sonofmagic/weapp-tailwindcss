267e5ea4db80c3d867b84b2d9d7fdadd694723d2
ice breaker <1324318532@qq.com>
2024-06-13 19:59:21 +0800

    chore(doc): misc docx script commit

--- website/scripts/docx.ts
+++ website/scripts/docx.ts
@@ -1,14 +1,16 @@
-const path = require('node:path')
-const fs = require('fs-extra')
-const klaw = require('klaw')
+import path from 'pathe'
+import fs from 'fs-extra'
+import klaw from 'klaw'
+import { execa } from 'execa'
 
 const targetDir = path.resolve(__dirname, 'docx')
 const sourceDocsDir = path.resolve(__dirname, '../docs')
 
 async function main() {
   await fs.ensureDir(targetDir)
-  const { execa } = await import('execa')
+  // const { execa } = await import('execa')
   for await (const file of klaw(sourceDocsDir)) {
+    file.path = path.normalize(file.path)
     const p = file.path.replace(sourceDocsDir, '')
     // const docxFilename = `${path.basename(p, '.md')}.docx`
     if (file.stats.isFile() && p.endsWith('.md') && !p.startsWith('/api')) {
