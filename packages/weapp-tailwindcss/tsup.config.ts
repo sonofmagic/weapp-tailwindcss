e37fe6811833a53c511fea9809f96b0ce13b9685
ice breaker <1324318532@qq.com>
2024-07-20 01:53:21 +0800

    feat: use tsup for build bundle

---
+++ packages/weapp-tailwindcss/tsup.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig } from 'tsup'
+
+export default defineConfig({
+  entry: {
+    'index': 'src/index.ts',
+    'webpack': 'src/webpack.ts',
+    'webpack4': 'src/webpack4.ts',
+    'gulp': 'src/gulp.ts',
+    'postcss': 'src/postcss.ts',
+    'cli': 'src/cli.ts',
+    'replace': 'src/replace.ts',
+    'vite': 'src/vite.ts',
+    'weapp-tw-runtime-loader': 'src/bundlers/webpack/loaders/weapp-tw-runtime-loader.ts',
+    'defaults': 'src/defaults.ts',
+    'css-macro/index': 'src/css-macro/index.ts',
+    'css-macro/postcss': 'src/css-macro/postcss.ts',
+    'core': 'src/core.ts',
+  },
+  dts: true,
+  clean: true,
+  format: ['cjs', 'esm'],
+  external: ['webpack', 'loader-utils', 'tailwindcss/plugin', '@ast-grep/napi', '@weapp-tailwindcss/cli'],
+})
