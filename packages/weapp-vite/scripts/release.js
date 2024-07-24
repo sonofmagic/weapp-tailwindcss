09a2c5652ca8b768489d658787ac20c429ba620d
ice breaker <1324318532@qq.com>
2024-07-24 21:07:53 +0800

    chore(weapp): init weapp-vite

---
+++ packages/weapp-vite/scripts/release.js
@@ -0,0 +1,28 @@
+const path = require('node:path')
+const fs = require('fs-extra')
+
+const pluginNames = ['weapp-vite', 'vite-plugin-weapp']
+
+// pnpm publish --access public --no-git-checks
+
+async function main() {
+  const { execa } = await import('execa')
+  const pkgJsonPath = path.resolve(__dirname, '../package.json')
+  const pkgJson = await fs.readJSON(pkgJsonPath)
+  const pkgName = pkgJson.name
+
+  for (const pluginName of pluginNames) {
+    try {
+      await fs.writeJSON(pkgJsonPath, { ...pkgJson, name: pluginName })
+      const { stdout } = await execa('pnpm', ['publish', '--access', 'public', '--no-git-checks'])
+      console.log(stdout)
+      // .pipeStdout(process.stdout)
+    }
+    catch (error) {
+      console.error(error)
+    }
+  }
+  await fs.writeJSON(pkgJsonPath, { ...pkgJson, name: pkgName }, { spaces: 2 })
+}
+
+main()
---
+++ packages/weapp-vite/src/index.ts
@@ -0,0 +1,10 @@
+import type { Plugin } from 'vite'
+// import { transformWithEsbuild } from 'vite'
+
+export default function vitePluginWeapp(): Plugin[] {
+  return [
+    {
+      name: 'vite-plugin-weapp',
+    },
+  ]
+}
