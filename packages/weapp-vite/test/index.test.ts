9fb39396187e328a089d9b65823824d7fbf39cae
ice breaker <1324318532@qq.com>
2024-07-25 22:04:07 +0800

    chore(test): add SOCKET_PATH for windows

--- packages/weapp-vite/test/index.test.ts
+++ packages/weapp-vite/test/index.test.ts
@@ -1,12 +1,19 @@
+import os from 'node:os'
+import path from 'node:path'
 import { createServer } from 'vite'
+import fs from 'fs-extra'
 import plugin from '@/index'
 
 describe('vite', () => {
   it('createServer', async () => {
     // const app = express()
     // app.listen()
-    const SOCKET_PATH = '/tmp/weapp-vite.sock'
-
+    const tmpdir = os.tmpdir()
+    console.log(`[weapp vite] tmpdir: ${tmpdir}`)
+    // × vite > createServer
+    //  → listen EACCES: permission denied /tmp/weapp-vite.sock
+    const SOCKET_PATH = path.resolve(tmpdir, 'weapp-vite.sock') // '/tmp/'
+    fs.ensureFileSync(SOCKET_PATH)
     const server = await createServer(
       {
         configFile: false,
