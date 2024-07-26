00822c961541457eb35f768847daa82c782afb18
ice breaker <1324318532@qq.com>
2024-07-26 13:41:20 +0800

    feat(bench): add 3d model viewer

---
+++ benchmark/src/pages/index.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div>
+    <model-viewer
+      class="h-screen w-screen"
+      src="blender.glb" ar
+      shadow-intensity="1" camera-controls touch-action="pan-y"
+    />
+  </div>
+</template>
+
+<style scoped></style>
