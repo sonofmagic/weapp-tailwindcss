1f871c9b1a6c809d95799f3e9ac38a8bacb4917b
ice breaker <1324318532@qq.com>
2024-08-03 15:41:50 +0800

    feat(vite): @weapp-core/init

--- packages/weapp-vite/src/cli.ts
+++ packages/weapp-vite/src/cli.ts
@@ -2,15 +2,15 @@ import process from 'node:process'
 import { program } from 'commander'
 import { runDev, runProd } from './build'
 
+const cwd = process.cwd()
+
 program
   .command('dev').action(async () => {
-    const cwd = process.cwd()
     await runDev(cwd)
   })
 
 program
   .command('build').action(async () => {
-    const cwd = process.cwd()
     await runProd(cwd)
   })
 
