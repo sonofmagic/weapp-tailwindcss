1e52fe4716963aca0777d8d42544aef18e42d15d
ice breaker <1324318532@qq.com>
2024-08-03 15:13:48 +0800

    feat(vite): add plugin

--- @weapp-core/shared/src/index.ts
+++ @weapp-core/shared/src/index.ts
@@ -1,2 +1,12 @@
 export { default as set } from 'set-value'
 export { default as get } from 'get-value'
+
+export function escapeStringRegexp(str: string) {
+  return str
+    .replace(/[|\\{}()[\]^$+*?.]/g, '\\$&')
+    .replace(/-/g, '\\x2d')
+}
+
+export function removeExtension(file: string) {
+  return file.replace(/\.[^./]+$/, '')
+}
