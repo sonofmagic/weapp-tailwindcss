20d90851e118b15660223619d2b00dd21eaed934
ice breaker <1324318532@qq.com>
2024-08-05 23:22:45 +0800

    feat(vite): commit init package

--- @weapp-core/init/test/index.test.ts
+++ @weapp-core/init/test/index.test.ts
@@ -1,6 +1,6 @@
 import path from 'pathe'
 import fs from 'fs-extra'
-import { updatePackageJson, updateProjectConfig } from '@/index'
+import { initViteConfigFile, updatePackageJson, updateProjectConfig } from '@/index'
 
 const appsDir = path.resolve(__dirname, '../../../apps')
 const fixturesDir = path.resolve(__dirname, './fixtures')
@@ -13,5 +13,34 @@ describe('index', () => {
     updateProjectConfig({ root, dest: p1 })
     expect(fs.existsSync(p0)).toBe(true)
     expect(fs.existsSync(p1)).toBe(true)
+  })
+
+  it.each(['vite-native', 'vite-native-skyline', 'vite-native-ts', 'vite-native-ts-skyline'])('%s callback', (name) => {
+    const root = path.resolve(appsDir, name)
+    const p0 = path.resolve(fixturesDir, name, 'package0.json')
+    const res0 = updatePackageJson({
+      root,
+      dest: p0,
+      command: 'weapp-vite',
+      write: false,
+      cb(set) {
+        set('type', 'module')
+      },
+    })
+    expect(res0).toMatchSnapshot()
+    const p1 = path.resolve(fixturesDir, name, 'project0.config.json')
+    const res1 = updateProjectConfig({ root, dest: p1, write: false })
+    expect(res1).toMatchSnapshot()
+    expect(fs.existsSync(p0)).toBe(false)
+    expect(fs.existsSync(p1)).toBe(false)
+  })
+
+  it.each(['vite-native', 'vite-native-skyline', 'vite-native-ts', 'vite-native-ts-skyline'])('%s vite.config.ts', (name) => {
+    const root = path.resolve(appsDir, name)
+    const res0 = initViteConfigFile({
+      root,
+      write: true,
+    })
+    expect(res0).toMatchSnapshot()
   })
 })
--- packages/weapp-vite/src/index.ts
+++ packages/weapp-vite/src/index.ts
@@ -1,9 +1,1 @@
-import type { UserConfig, UserConfigExport, UserConfigFnObject } from 'vite'
-
-export function defineConfig(config: UserConfig): UserConfig
-export function defineConfig(config: Promise<UserConfig>): Promise<UserConfig>
-export function defineConfig(config: UserConfigFnObject): UserConfigFnObject
-export function defineConfig(config: UserConfigExport): UserConfigExport
-export function defineConfig(config: UserConfigExport): UserConfigExport {
-  return config
-}
+export * from './build'
