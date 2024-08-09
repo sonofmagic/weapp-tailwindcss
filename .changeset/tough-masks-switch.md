2e458bb8ecce0fdb81de47982a8577f07739fa9e
ice breaker <1324318532@qq.com>
2024-08-10 02:34:54 +0800

    chore: bump version

---
+++ .changeset/tough-masks-switch.md
@@ -0,0 +1,5 @@
+---
+"weapp-vite": patch
+---
+
+fix: Cannot find module `weapp-vite/config` error
--- packages/weapp-vite/tsup.config.ts
+++ packages/weapp-vite/tsup.config.ts
@@ -4,6 +4,7 @@ export default defineConfig({
   entry: {
     index: 'src/index.ts',
     cli: 'src/cli.ts',
+    config: 'src/config.ts',
   },
   dts: true,
   clean: true,
