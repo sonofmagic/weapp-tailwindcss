cefa2472bf2dc044d5e23756eabdeb727d5592c5
ice breaker <1324318532@qq.com>
2024-09-21 00:28:34 +0800

    chore: upgrade monorepo cli manager

--- scripts/monorepo/init.ts
+++
@@ -1,8 +0,0 @@
-import { createContext } from './context'
-import setPkgJson from './setPkgJson'
-import setReadme from './setReadme'
-
-const ctx = await createContext()
-
-await setPkgJson(ctx)
-await setReadme(ctx)
