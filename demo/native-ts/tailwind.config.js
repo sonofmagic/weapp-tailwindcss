80801f8919cb2fc17e3044a5e5cebbb0574e5491
ice breaker <1324318532@qq.com>
2024-10-20 01:37:07 +0800

    chore(demo): upgrade deps

--- demo/native-ts/tailwind.config.js
+++ demo/native-ts/tailwind.config.js
@@ -1,9 +1,15 @@
+import path from 'path'
+
 /** @type {import('tailwindcss').Config} */
 export default {
   important: '#app',
   // 这里给出了一份 uni-app /taro 通用示例，具体要根据你自己项目的目录结构进行配置
   // 不在 content 包括的文件内，你编写的 class，是不会生成对应的css工具类的
-  content: ['./public/index.html', './miniprogram/**/*.{wxml,html,js,ts,jsx,tsx,vue}'],
+  content: [
+    './public/index.html',
+    './miniprogram/**/*.{wxml,html,js,ts,jsx,tsx,vue}',
+    path.resolve(__dirname, '../../packages/tailwindcss-core-plugins-extractor/src/**/*.ts')
+  ],
   // 其他配置项
   // ...
   corePlugins: {
