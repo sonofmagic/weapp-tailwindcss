75ee97c35cb3401bee74c679a07aefeca4c8aebb
ice breaker <1324318532@qq.com>
2025-01-07 22:23:36 +0800

    chore: add cn and cva

--- packages/merge/src/index.ts
+++ packages/merge/src/index.ts
@@ -1,12 +1,20 @@
+import type { ClassProp } from 'class-variance-authority/types'
 import { cva as _cva } from 'class-variance-authority'
 import { type ClassValue, clsx } from 'clsx'
 import { twMerge } from 'tailwind-merge'
-import { escape } from 'weapp-tailwindcss/escape'
+import { escape, weappTwIgnore } from 'weapp-tailwindcss/escape'
 
 export function cn(...inputs: ClassValue[]) {
   return escape(twMerge(clsx(inputs)))
 }
 
-export function cva(...inputs: Parameters<typeof cva>) {
-  return escape(_cva(...inputs))
+export function cva(...inputs: Parameters<typeof _cva>) {
+  const fn = _cva(...inputs)
+  return (props?: ClassProp | undefined) => {
+    return escape(fn(props))
+  }
+}
+
+export {
+  weappTwIgnore,
 }
