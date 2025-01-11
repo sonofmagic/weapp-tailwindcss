1df699bf82d66847cbc64cc8f294ef237e0470c5
ice breaker <1324318532@qq.com>
2025-01-11 12:19:39 +0800

    chore: bump version

---
+++ .changeset/soft-zoos-greet.md
@@ -0,0 +1,6 @@
+---
+"@weapp-tailwindcss/postcss": patch
+"@weapp-tailwindcss/mangle": patch
+---
+
+chore: fix types import intel
--- packages/postcss/src/index.ts
+++ packages/postcss/src/index.ts
@@ -1,28 +1,4 @@
-import type { IStyleHandlerOptions } from './types'
-import { defuOverrideArray } from '@weapp-tailwindcss/shared'
-import postcss from 'postcss'
-import { getPlugins } from './plugins'
-
+export * from './handler'
 export { createInjectPreflight } from './preflight'
-
 export { internalCssSelectorReplacer } from './shared'
-
-export function styleHandler(rawSource: string, options: IStyleHandlerOptions) {
-  return postcss(getPlugins(options))
-    .process(
-      rawSource,
-      options.postcssOptions?.options ?? {
-        from: undefined,
-      },
-    )
-    .async()
-}
-
-export function createStyleHandler(options: Partial<IStyleHandlerOptions>) {
-  return (rawSource: string, opt?: Partial<IStyleHandlerOptions>) => {
-    return styleHandler(
-      rawSource,
-      defuOverrideArray<IStyleHandlerOptions, Partial<IStyleHandlerOptions>[]>(opt as IStyleHandlerOptions, options),
-    )
-  }
-}
+export * from './types'
