5187b9a6c6e086c5b193d43c9ebfa61b5cb1de6f
ice breaker <1324318532@qq.com>
2025-01-15 19:27:42 +0800

    chore: remove light-effect

--- website/src/theme/Layout.tsx
+++ website/src/theme/Layout.tsx
@@ -1,13 +1,17 @@
-import { useLocation } from '@docusaurus/router'
+// import { useLocation } from '@docusaurus/router'
 import Layout from '@theme-original/Layout'
 import React from 'react'
 
 export default function LayoutWrapper(props) {
-  const location = useLocation()
+  // const location = useLocation()
 
   return (
     <div className="relative">
-      {location.pathname !== '/' && <div className="light-effect pointer-events-none absolute right-[13.14%] z-[201]"></div>}
+      {/* {location.pathname !== '/' && (
+        <div className="pointer-events-none absolute inset-0 z-[201]">
+          <div className="light-effect pointer-events-none absolute right-[13.14%]"></div>
+        </div>
+      )} */}
       <Layout {...props} />
       <div className="pointer-events-none absolute inset-0 z-[202] flex-none">
         <div className="size-full rounded-none bg-[url(/img/framer.png)] bg-[length:128px] bg-repeat opacity-5">
