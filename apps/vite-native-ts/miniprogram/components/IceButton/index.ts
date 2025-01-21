db56d1b936b3c07a7f333fc936824f900548bdf2
ice breaker <1324318532@qq.com>
2025-01-22 04:01:48 +0800

    chore(deps): upgrade

--- apps/vite-native-ts/miniprogram/components/IceButton/index.ts
+++ apps/vite-native-ts/miniprogram/components/IceButton/index.ts
@@ -1,4 +1,4 @@
-import { cn } from '@weapp-tailwindcss/merge'
+import { twMerge } from '@weapp-tailwindcss/merge'
 
 const defaultClass = 'weapp-reset-button px-6 py-2 font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue-600 rounded-lg hover:bg-blue-500 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-80'
 
@@ -36,7 +36,7 @@ Component({
   observers: {
     twClass(val) {
       this.setData({
-        mergedClass: cn(defaultClass, val),
+        mergedClass: twMerge(defaultClass, val),
       })
     },
   },
