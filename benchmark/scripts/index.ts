1e097322fc1ec2a42e2a97e9341c29c1c8f64d21
ice breaker <1324318532@qq.com>
2025-02-01 21:30:03 +0800

    chore(deps): upgrade

---
+++ benchmark/scripts/index.ts
@@ -0,0 +1,18 @@
+import tailwindcss from '@tailwindcss/postcss'
+import fs from 'fs-extra'
+import path from 'pathe'
+import postcss from 'postcss'
+
+async function main() {
+  const { css } = await postcss([
+    tailwindcss({
+
+    }),
+  ]).process('@import "tailwindcss";', {
+    from: './index.ts',
+  })
+
+  await fs.outputFile(path.resolve(import.meta.dirname, '../../packages/weapp-tailwindcss/test/fixtures/css/v4-default.css'), css, 'utf8')
+}
+
+main()
