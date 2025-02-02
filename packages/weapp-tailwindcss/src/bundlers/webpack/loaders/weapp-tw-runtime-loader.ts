3da864338de73a304346fd47b4a91fa18d9f3163
ice breaker <1324318532@qq.com>
2025-02-03 01:45:01 +0800

    chore: bump version

--- packages/weapp-tailwindcss/src/bundlers/webpack/loaders/weapp-tw-runtime-loader.ts
+++ packages/weapp-tailwindcss/src/bundlers/webpack/loaders/weapp-tw-runtime-loader.ts
@@ -1,14 +1,11 @@
 // @ts-nocheck
 import type webpack from 'webpack'
-
-// eslint-disable-next-line ts/no-require-imports
-const loaderUtils = require('loader-utils')
-// import loaderUtils from 'loader-utils'
+import loaderUtils from 'loader-utils'
 
 const WeappTwRuntimeAopLoader: webpack.LoaderDefinitionFunction<{
   getClassSet: () => void
 }> = function (this: webpack.LoaderContext<any>, source: string) {
-  const opt = loaderUtils.getCompilerContext(this) // this.getCompilerContext()
+  const opt = loaderUtils.getOptions(this) // this.getCompilerContext()
   opt?.getClassSet?.()
   return source
 }
