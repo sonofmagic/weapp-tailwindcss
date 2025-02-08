ce8851a057475e2d8eb20b6c4bfa15642b3b1bae
ice breaker <1324318532@qq.com>
2025-02-09 00:29:43 +0800

    feat: add weapp-tailwindcss style

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,7 +1,8 @@
 import { icebreaker } from '@icebreakers/stylelint-config'
 
 export default icebreaker({
-  ignorePatterns: [
-    '**/packages/weapp-tailwindcss/test/fixtures/css/**',
+  ignoreFiles: [
+    'packages/weapp-tailwindcss/test/fixtures/css/**',
+    'packages/weapp-tailwindcss/*.css',
   ],
 })
