fe675d0a75a8da581b0b2b19cef217b0a5160120
ice breaker <1324318532@qq.com>
2025-02-20 11:52:41 +0800

    chore: init @weapp-tailwindcss/babel

---
+++ packages/babel/src/babel.ts
@@ -0,0 +1,12 @@
+// import _babelGenerate from '@babel/generator'
+import _babelTraverse from '@babel/traverse'
+
+export { parse, parseExpression } from '@babel/parser'
+
+function _interopDefaultCompat(e: any) {
+  return e && typeof e === 'object' && 'default' in e ? e.default : e
+}
+
+// export const generate = _interopDefaultCompat(_babelGenerate) as typeof _babelGenerate
+
+export const traverse = _interopDefaultCompat(_babelTraverse) as typeof _babelTraverse
---
+++ packages/babel/test/index.test.ts
@@ -0,0 +1,5 @@
+describe('index', () => {
+  it('foo bar', () => {
+    expect(1).toBe(1)
+  })
+})
