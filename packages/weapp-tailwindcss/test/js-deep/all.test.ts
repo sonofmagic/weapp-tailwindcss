6c98b0d26915915739ff0277e0ed3c01b7b4bbd7
ice breaker <1324318532@qq.com>
2025-03-13 01:07:21 +0800

    chore(deps): upgrade

--- packages/weapp-tailwindcss/test/js-deep/all.test.ts
+++ packages/weapp-tailwindcss/test/js-deep/all.test.ts
@@ -1,3 +1,4 @@
+import type { ImportToken } from '@/js/NodePathWalker'
 import { parse } from '@/babel'
 import { analyzeSource } from '@/js/babel'
 import fs from 'fs-extra'
@@ -25,8 +26,12 @@ describe('all', () => {
       return {
         ms: new MagicString(code),
         ast: babelParse(code),
+        tokens: [] as ImportToken[],
+        imports: {},
+        analysis: {} as ReturnType<typeof analyzeSource>,
       }
     }
+    // const base = '.'
     const sp = {
       'a.js': init(getCase('import/a.js')),
       'shared.js': init(getCase('import/shared.js')),
@@ -34,15 +39,12 @@ describe('all', () => {
     }
 
     {
+      // dirname
       const { exportDeclarations, walker } = analyzeSource(sp['a.js'].ast, {
         ignoreCallExpressionIdentifiers,
       })
       expect(exportDeclarations.size).toBe(0)
       expect(walker.imports.size).toBe(3)
-
-      // for (const importee of walker.imports) {
-
-      // }
     }
 
     {
@@ -51,6 +53,7 @@ describe('all', () => {
       })
       expect(exportDeclarations.size).toBe(5)
       expect(walker.imports.size).toBe(0)
+      // const exportWalker = new NodePathWalker()
     }
 
     {
@@ -60,6 +63,56 @@ describe('all', () => {
       expect(exportDeclarations.size).toBe(2)
       expect(walker.imports.size).toBe(0)
     }
+  })
+
+  it('all code 1', () => {
+    function init(code: string) {
+      return {
+        ms: new MagicString(code),
+        ast: babelParse(code),
+        tokens: [] as ImportToken[],
+        imports: {},
+        analysis: {} as ReturnType<typeof analyzeSource>,
+      }
+    }
+    // const base = '.'
+    const sp = {
+      'a.js': init(getCase('import/a.js')),
+      'shared.js': init(getCase('import/shared.js')),
+      'shared2.js': init(getCase('import/shared2.js')),
+    }
+
+    for (const [, entry] of Object.entries(sp)) {
+      entry.analysis = analyzeSource(entry.ast, {
+        ignoreCallExpressionIdentifiers,
+      })
+    }
+
+    for (const [, entry] of Object.entries(sp)) {
+      entry.imports = Array.from(
+        entry.analysis.walker.imports,
+      ).reduce<Record<string, ImportToken[]>>(
+        (acc, token) => {
+          // entry.tokens.push(token)
+          if (Array.isArray(acc[token.source])) {
+            acc[token.source].push(token)
+          }
+          else {
+            acc[token.source] = [token]
+          }
+          for (const [x, xx] of Object.entries(sp)) {
+            // @ts-ignore
+            if (`${token.source}.js` === `./${x}`) {
+              xx.tokens.push(token)
+            }
+          }
+
+          return acc
+        },
+        {},
+      )
+    }
+    expect(sp).toBeTruthy()
 
     // expect(ms.toString()).toBe(`import { a as bb } from './shared'\n\ncn(bb, "__", "yy")`)
     // expect(walker.imports.size).toBe(3)
