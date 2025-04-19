69e19630517b690879943ef9e0a4f4354f132594
ice breaker <1324318532@qq.com>
2025-04-20 00:49:33 +0800

    chore(deps): upgrade

--- apps/uni-app-x-hbuilderx-tailwindcss4/vite.config.ts
+++ apps/uni-app-x-hbuilderx-tailwindcss4/vite.config.ts
@@ -1,8 +1,18 @@
 import uni from '@dcloudio/vite-plugin-uni'
 import tailwindcss from '@tailwindcss/postcss'
+import fs from 'fs-extra'
+import path from 'pathe'
 import { defineConfig } from 'vite'
 import { uniAppX } from 'weapp-tailwindcss/presets'
 import { UnifiedViteWeappTailwindcssPlugin } from 'weapp-tailwindcss/vite'
+
+function n(id: string) {
+  const idx = id.indexOf('node_modules')
+  if (idx > -1) {
+    return id.slice(idx)
+  }
+  return path.relative(__dirname, id)
+}
 
 export default defineConfig({
   plugins: [
@@ -13,6 +23,28 @@ export default defineConfig({
         rem2rpx: true,
       }),
     ),
+    {
+      name: 'weapp-tailwindcss:debug:post',
+      enforce: 'pre',
+      transform(code, id) {
+        fs.outputFileSync(
+          path.join(__dirname, 'debug', 'pre', n(id)),
+          code,
+          'utf8',
+        )
+      },
+    },
+    {
+      name: 'weapp-tailwindcss:debug:post',
+      enforce: 'post',
+      transform(code, id) {
+        fs.outputFileSync(
+          path.join(__dirname, 'debug', 'post', n(id)),
+          code,
+          'utf8',
+        )
+      },
+    },
   ],
   css: {
     postcss: {
