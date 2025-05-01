27c76fb63f2399fc4a31ff51cb1b17cc194e80a0
ice breaker <1324318532@qq.com>
2025-05-01 14:24:52 +0800

    chore(demo): upgrade

--- apps/vite-native-ts/vite.config.ts
+++ apps/vite-native-ts/vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
   weapp: {
     enhance: {
       autoImportComponents: {
-        globs: ['miniprogram/components/**/*'],
+        globs: ['components/**/*'],
       },
     },
     // weapp-vite options
