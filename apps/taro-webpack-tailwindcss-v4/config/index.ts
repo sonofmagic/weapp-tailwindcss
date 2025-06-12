3cd36bd6d2dea00283b46867c9b22c534e87800c
ice breaker <1324318532@qq.com>
2025-06-12 16:21:18 +0800

    chore(deps): upgrade

--- apps/taro-webpack-tailwindcss-v4/config/index.ts
+++ apps/taro-webpack-tailwindcss-v4/config/index.ts
@@ -29,9 +29,20 @@ export default defineConfig<'webpack5'>(async (merge, { command, mode }) => {
       },
     },
     framework: 'react',
-    compiler: 'webpack5',
+    compiler: {
+      type: 'webpack5',
+      prebundle: {
+        enable: false,
+      },
+    },
     cache: {
       enable: false, // Webpack 持久化缓存配置，建议开启。默认配置请参考：https://docs.taro.zone/docs/config-detail#cache
+    },
+    terser: {
+      enable: true,
+      config: {
+
+      },
     },
     mini: {
       postcss: {
