4d57a2e6d8be1957f6d3f759b84de2b506c6d0f6
ice breaker <1324318532@qq.com>
2025-06-13 13:54:02 +0800

    chore: add workers_dev

--- website/worker/index.ts
+++ website/worker/index.ts
@@ -1,6 +1,13 @@
 export default {
-  async fetch(request, env) {
-    const file = await env.ASSETS.fetch(request)
-    return file
+  fetch(request, env) {
+    // const url = new URL(request.url)
+
+    // if (url.pathname.startsWith('/api/')) {
+    //   return new Response(JSON.stringify({ name: 'Cloudflare' }), {
+    //     headers: { 'Content-Type': 'application/json' },
+    //   })
+    // }
+
+    return env.ASSETS.fetch(request)
   },
 } satisfies ExportedHandler<CloudflareBindings>
