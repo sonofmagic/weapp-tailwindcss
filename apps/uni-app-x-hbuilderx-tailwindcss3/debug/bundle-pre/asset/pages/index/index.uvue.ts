5add719839a261a3664457b1ee75f2cb8f024dbc
ice breaker <1324318532@qq.com>
2025-07-21 21:55:44 +0800

    chore: commit uni-app-x debugger

---
+++ apps/uni-app-x-hbuilderx-tailwindcss3/debug/bundle-pre/asset/pages/index/index.uvue.ts
@@ -0,0 +1,27 @@
+
+	const __sfc__ = defineComponent({
+		data() {
+			return {
+				title: 'Hello'
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	})
+
+export default __sfc__
+function GenPagesIndexIndexRender(this: InstanceType<typeof __sfc__>): any | null {
+const _ctx = this
+const _cache = this.$.renderCache
+  return _cE("view", _uM({ class: "px-4" }), [
+    _cE("view", _uM({ class: "mt-[32.43rpx] bg-[#322323]" }), [
+      _cE("text", _uM({ class: "text-[#844343]" }), _tD(_ctx.title), 1 /* TEXT */)
+    ]),
+    _cE("text", _uM({ class: "icebreaker" }), "icebreaker")
+  ])
+}
+/*GenPagesIndexIndexStyles*/
