d3cb8cc0da85f7fea052873367cf4004d1f4c514
ice breaker <1324318532@qq.com>
2025-08-12 23:27:01 +0800

    chore(deps): upgrade

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -1,5 +1,4 @@
 import type { UserConfig } from '@commitlint/types'
-// import { RuleConfigSeverity } from '@commitlint/types'
 
 export default <UserConfig>{
   extends: ['@commitlint/config-conventional'],
