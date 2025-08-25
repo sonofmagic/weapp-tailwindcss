3275cbd94dcabba51dc556f8b03452b5440e21a8
ice breaker <1324318532@qq.com>
2025-08-25 22:39:30 +0800

    chore(postcss): add dark mode case

---
+++ packages/postcss/test/darkMode.test.ts
@@ -0,0 +1,84 @@
+import fs from 'fs-extra'
+import path from 'pathe'
+import { createStyleHandler } from '@/index'
+
+function getCase(name: string) {
+  return fs.readFile(path.resolve(import.meta.dirname, `./fixtures/css/darkMode/${name}`), 'utf8')
+}
+
+describe('darkMode', () => {
+  it(`darkMode: 'class',`, async () => {
+    const styleHandler = createStyleHandler({
+      isMainChunk: true,
+    })
+    const code = await getCase('class.css')
+    const { css } = await styleHandler(code)
+    expect(css).toMatchSnapshot()
+  })
+
+  it(`darkMode: 'class', uni-app x`, async () => {
+    const styleHandler = createStyleHandler({
+      isMainChunk: true,
+      uniAppX: true,
+    })
+    const code = await getCase('class.css')
+    const { css } = await styleHandler(code)
+    expect(css).toMatchSnapshot()
+  })
+
+  it(`darkMode: 'selector',`, async () => {
+    const styleHandler = createStyleHandler({
+      isMainChunk: true,
+    })
+    const code = await getCase('selector.css')
+    const { css } = await styleHandler(code)
+    expect(css).toMatchSnapshot()
+  })
+
+  it(`darkMode: 'selector', uni-app x`, async () => {
+    const styleHandler = createStyleHandler({
+      isMainChunk: true,
+      uniAppX: true,
+    })
+    const code = await getCase('selector.css')
+    const { css } = await styleHandler(code)
+    expect(css).toMatchSnapshot()
+  })
+
+  it(`darkMode: ['selector', '[data-mode="dark"]'],`, async () => {
+    const styleHandler = createStyleHandler({
+      isMainChunk: true,
+    })
+    const code = await getCase('attr.css')
+    const { css } = await styleHandler(code)
+    expect(css).toMatchSnapshot()
+  })
+
+  it(`darkMode: ['selector', '[data-mode="dark"]'], uni-app x`, async () => {
+    const styleHandler = createStyleHandler({
+      isMainChunk: true,
+      uniAppX: true,
+    })
+    const code = await getCase('attr.css')
+    const { css } = await styleHandler(code)
+    expect(css).toMatchSnapshot()
+  })
+
+  it(`darkMode: ['variant', '&:not(.light *)'],`, async () => {
+    const styleHandler = createStyleHandler({
+      isMainChunk: true,
+    })
+    const code = await getCase('variant0.css')
+    const { css } = await styleHandler(code)
+    expect(css).toMatchSnapshot()
+  })
+
+  it(`darkMode: ['variant', '&:not(.light *)'], uni-app x`, async () => {
+    const styleHandler = createStyleHandler({
+      isMainChunk: true,
+    })
+    const code = await getCase('variant0.css')
+    const { css } = await styleHandler(code)
+    expect(css).toMatchSnapshot()
+  })
+})
