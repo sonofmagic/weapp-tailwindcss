8a69174b98250eb596bdb38573576f07cd1b53e2
ice breaker <1324318532@qq.com>
2025-09-16 22:58:14 +0800

    chore: add cssCalc

--- demo/taro-webpack-tailwindcss-v4/config/index.ts
+++ demo/taro-webpack-tailwindcss-v4/config/index.ts
@@ -2,7 +2,7 @@ import { defineConfig, type UserConfigExport } from '@tarojs/cli'
 import TsconfigPathsPlugin from 'tsconfig-paths-webpack-plugin'
 import devConfig from './dev'
 import prodConfig from './prod'
-import { UnifiedWebpackPluginV5 } from 'weapp-tailwindcss/webpack'
+import { UnifiedWebpackPluginV5, UserDefinedOptions } from 'weapp-tailwindcss/webpack'
 
 // https://taro-docs.jd.com/docs/next/config#defineconfig-辅助函数
 export default defineConfig<'webpack5'>(async (merge, { command, mode }) => {
@@ -73,7 +73,10 @@ export default defineConfig<'webpack5'>(async (merge, { command, mode }) => {
               args: [
                 {
                   rem2rpx: true,
-                }
+                  // before 2248
+                  // after 309
+                  // cssCalc: ['--nutui']
+                } satisfies UserDefinedOptions
               ]
             }
           }
