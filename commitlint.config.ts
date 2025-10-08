b636937f41cc06df849ad84441afd3fcbc0aeac6
ice breaker <1324318532@qq.com>
2025-10-08 08:26:05 +0800

    chore(deps): upgrade

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -1,5 +1,3 @@
-import type { UserConfig } from '@commitlint/types'
+import { icebreaker } from '@icebreakers/commitlint-config'
 
-export default <UserConfig>{
-  extends: ['@commitlint/config-conventional'],
-}
+export default icebreaker()
