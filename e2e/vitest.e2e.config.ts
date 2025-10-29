b68f75e42351f2744ca72d2ea1997797e06f6871
ice breaker <1324318532@qq.com>
2025-10-29 20:46:42 +0800

    chore(demo): snap update

--- e2e/vitest.e2e.config.ts
+++ e2e/vitest.e2e.config.ts
@@ -6,16 +6,7 @@ export default defineConfig({
     include: [path.resolve(__dirname, './*.test.ts')],
     testTimeout: 36_000_000,
     globals: true,
-    poolOptions: {
-      forks: {
-        maxForks: 1,
-        minForks: 1,
-      },
-      threads: {
-        maxThreads: 1,
-        minThreads: 1,
-      },
-    },
-    // ...
+    maxConcurrency: 1,
+    maxWorkers: 1,
   },
 })
