7380005805697c2d75ce1c3c9d32564b68f1879a
ice breaker <1324318532@qq.com>
2025-11-19 01:07:52 +0800

    chore: sync:templates

--- templates/taro-webpack-tailwindcss-v4/src/pages/index/index.tsx
+++ templates/taro-webpack-tailwindcss-v4/src/pages/index/index.tsx
@@ -9,8 +9,7 @@ export default function Index() {
 
   return (
     <View className='min-h-screen'>
-
-      <View className='bg-[#534312] text-[#fff] text-[100rpx] text-center rounded-t-full'>
+      <View className='bg-[#534312] text-[#fff] text-[100rpx] text-center rounded-t-full h-1/2'>
         <Text>Hello world!</Text>
       </View>
       <View className='text-[#16b6c4] text-[34.32rpx] text-center mt-[124.432rpx]'>欢迎使用 weapp-tailwindcss 模板</View>
