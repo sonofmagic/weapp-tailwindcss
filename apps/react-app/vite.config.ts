6cafce3fe8f5c3a13fa1da1fc5a9c6671b649014
ice breaker <1324318532@qq.com>
2025-11-25 14:29:00 +0800

    chore: add shadcn ui

--- apps/react-app/vite.config.ts
+++ apps/react-app/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+import path from "path"
+import tailwindcss from "@tailwindcss/vite"
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
@@ -9,5 +10,11 @@ export default defineConfig({
         plugins: [['babel-plugin-react-compiler']],
       },
     }),
+    tailwindcss()
   ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
