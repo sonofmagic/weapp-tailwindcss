7d49b7c1a6b0545e1aa87dfdffe6bcb42f2aa07c
ice breaker <1324318532@qq.com>
2025-12-23 20:29:10 +0800

    chore: @weapp-tailwindcss-demo/uni-app-webpack5

--- demo/uni-app/scripts/run-vue-cli-service.js
+++ demo/uni-app/scripts/run-vue-cli-service.js
@@ -1,5 +1,17 @@
 #!/usr/bin/env node
 
+const { resolve } = require('path')
+const Module = require('module')
+
+const uniCliSharedUtil = resolve(__dirname, '../node_modules/@dcloudio/uni-cli-shared/lib/util.js')
+const originalRequire = Module.prototype.require
+Module.prototype.require = function patchedRequire(id, ...args) {
+  if (id === '@dcloudio/uni-cli-shared/lib/util') {
+    return originalRequire.call(this, uniCliSharedUtil)
+  }
+  return originalRequire.apply(this, [id, ...args])
+}
+
 require('./patch-ajv-keywords')
 require('./patch-chalk')
 require('@vue/cli-service/bin/vue-cli-service')
