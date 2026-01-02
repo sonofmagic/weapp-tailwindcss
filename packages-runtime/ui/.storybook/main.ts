c279bf144db67d17f39e29a7c24e4fd121cc5716
ice breaker <1324318532@qq.com>
2026-01-02 13:32:55 +0800

    chore(deps): upgrade

--- packages-runtime/ui/.storybook/main.ts
+++ packages-runtime/ui/.storybook/main.ts
@@ -9,9 +9,7 @@ const config: StorybookConfig = {
   stories: ['../stories/**/*.mdx', '../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
 
   addons: [
-    '@storybook/addon-essentials',
-    '@storybook/addon-interactions',
-    '@storybook/addon-a11y',
+
   ],
 
   framework: {
