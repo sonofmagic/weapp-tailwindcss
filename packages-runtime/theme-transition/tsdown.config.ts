64dcb7450ca695aa0bd753fe8268252bdbc1a552
ice breaker <1324318532@qq.com>
2026-01-25 13:15:42 +0800

    fix: ci/cd

--- packages-runtime/theme-transition/tsdown.config.ts
+++ packages-runtime/theme-transition/tsdown.config.ts
@@ -6,7 +6,8 @@ export default defineConfig({
   format: ['cjs', 'esm'],
   clean: true,
   dts: true,
-  external: ['tailwindcss'],
+  external: [/^tailwindcss(\/|$)/],
+  inlineOnly: ['defu'],
   target: 'es6',
   // https://github.com/egoist/tsdown/pull/1056
   // https://github.com/egoist/tsdown/issues?q=cjsInterop
