3a268739b5d4450ba34267fe29467901af6c07e0
ice breaker <1324318532@qq.com>
2026-03-11 15:47:00 +0800

    feat(weapp-tailwindcss): 为所有编译插件入口新增 weappTailwindcss 别名导出

--- packages/weapp-tailwindcss/src/vite.ts
+++ packages/weapp-tailwindcss/src/vite.ts
@@ -1,2 +1,5 @@
 export * from './bundlers/vite/index'
+/** {@link UnifiedViteWeappTailwindcssPlugin} 的别名，方便简写引用 */
+export { UnifiedViteWeappTailwindcssPlugin as weappTailwindcss } from './bundlers/vite/index'
+
 export type { UserDefinedOptions } from './types'
