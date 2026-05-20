797bd7fa4a45b66c075f54679f611dac318d8e43
ice breaker <1324318532@qq.com>
2026-05-20 10:24:11 +0800

    fix: restore uni-app demo text size

--- demo/uni-app-vite-tailwindcss-v4/src/pages/index/index.vue
+++ demo/uni-app-vite-tailwindcss-v4/src/pages/index/index.vue
@@ -2,7 +2,7 @@
   <view class="flex flex-col">
     <image class="w-20 h-20" src="/static/logo.png" />
     <view class="text-area aspect-(--my-aspect-ratio)">
-      <text class="text-[#00f285] text-[128rpx] font-bold underline">{{ title }}</text>
+      <text class="text-[#00f285] text-[88rpx] font-bold underline">{{ title }}</text>
     </view>
     <view :class="className" class="aspect-[calc(4*3+1)/3]">
       {{ className }}
